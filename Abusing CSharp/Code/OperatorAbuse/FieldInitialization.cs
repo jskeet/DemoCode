6d92b7cdf9ad00bbbcfa7ce40e98d9cf65e23005
Jon Skeet <skeet@pobox.com>
2013-08-03 09:20:28 +0100

    Added OperatorAbuse project

---
+++ Abusing CSharp/Code/OperatorAbuse/FieldInitialization.cs
@@ -0,0 +1,45 @@
+﻿// Copyright 2013 Jon Skeet. All rights reserved. Use of this source code is governed by the Apache License 2.0, as found in the LICENSE.txt file.
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Runtime.CompilerServices;
+
+namespace OperatorAbuse
+{
+    class FieldInitialization
+    {
+        private GenericType<Dictionary<string, int>, IDynamicMetaObjectProvider, ICriticalNotifyCompletion> foo;
+        private Dictionary<IDynamicMetaObjectProvider, ICriticalNotifyCompletion> bar;
+
+        public FieldInitialization()
+        {
+            // Would rather not have this...
+            foo = new GenericType<Dictionary<string, int>, IDynamicMetaObjectProvider, ICriticalNotifyCompletion>();
+
+            // Can have this instead!
+            foo = +foo;
+
+            // Would rather not have this...
+            bar = new Dictionary<IDynamicMetaObjectProvider, ICriticalNotifyCompletion>();
+
+            // Can have this instead...
+            bar = bar.New();
+        }
+    }
+
+    public static class Extensions
+    {
+        public static Dictionary<TKey, TValue> New<TKey, TValue>(this Dictionary<TKey, TValue> ignored)
+        {
+            return new Dictionary<TKey, TValue>();
+        }
+    }
+
+    class GenericType<T1, T2, T3>
+    {
+        public static GenericType<T1, T2, T3> operator +(GenericType<T1, T2, T3> ignored)
+        {
+            return new GenericType<T1, T2, T3>();
+        }
+    }
+
+}
---
+++ Abusing CSharp/Code/OperatorAbuse/NamedOperators.cs
@@ -0,0 +1,74 @@
+﻿// Copyright 2013 Jon Skeet. All rights reserved. Use of this source code is governed by the Apache License 2.0, as found in the LICENSE.txt file.
+// Based on ideas by Konrad Rudolph: https://github.com/klmr/named-operator
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OperatorAbuse
+{
+    class NamedOperators
+    {
+        static void Main()
+        {
+            var repeat = Operators.Repeat<string>();
+            var join = Operators.Join<string>();
+            var result = "Hello" <repeat> 3 <join> ",";
+            Console.WriteLine(result);
+        }
+    }
+
+    public static class Operators
+    {
+        public static Operator<T, int, IEnumerable<T>> Repeat<T>()
+        {
+            return new Operator<T, int, IEnumerable<T>>(Enumerable.Repeat);
+        }
+
+        public static Operator<IEnumerable<T>, string, string> Join<T>()
+        {
+            return new Operator<IEnumerable<T>, string, string>((values, separator) => string.Join(separator, values));
+        }
+    }
+
+    public class Operator<TLeft, TRight, TResult>
+    {
+        private readonly Func<TLeft, TRight, TResult> func;
+
+        public Operator(Func<TLeft, TRight, TResult> func)
+        {
+            this.func = func;
+        }
+        
+        public static PartialOperator<TLeft, TRight, TResult> operator <(TLeft lhs, Operator<TLeft, TRight, TResult> op)
+        {
+            return new PartialOperator<TLeft, TRight, TResult>(lhs, op.func);
+        }
+
+        public static PartialOperator<TLeft, TRight, TResult> operator >(TLeft lhs, Operator<TLeft, TRight, TResult> op)
+        {
+            return new PartialOperator<TLeft, TRight, TResult>(lhs, op.func);
+        }
+    }
+
+    public class PartialOperator<TLeft, TRight, TResult>
+    {
+        private readonly Func<TLeft, TRight, TResult> func;
+        private readonly TLeft left;
+
+        internal PartialOperator(TLeft left, Func<TLeft, TRight, TResult> func)
+        {
+            this.left = left;
+            this.func = func;
+        }
+
+        public static TResult operator >(PartialOperator<TLeft, TRight, TResult> op, TRight rhs)
+        {
+            return op.func(op.left, rhs);
+        }
+
+        public static TResult operator <(PartialOperator<TLeft, TRight, TResult> op, TRight rhs)
+        {
+            return op.func(op.left, rhs);
+        }
+    }
+}
