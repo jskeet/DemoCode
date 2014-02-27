0f6ea7d5710eb73b9d147637ef3d87a30208ef8e
Jon Skeet <skeet@pobox.com>
2014-02-27 20:12:40 +0000

    Add preprocessor oddity examples

---
+++ Abusing CSharp/Code/OddsAndEnds/PreprocessorOddities1.cs
@@ -0,0 +1,24 @@
+﻿#define FOO
+using System;
+using System.ComponentModel;
+
+namespace OddsAndEnds
+{
+    [Description("Multi-line comments")]
+    class PreprocessorOddities1
+    {
+        static void Main()
+        {
+#if FOO
+            Console.WriteLine("FOO is defined");
+            /* This is a comment
+#else
+            Console.WriteLine("FOO is not defined");
+            /*/
+#else
+            /*/
+            Console.WriteLine("FOO is still not defined");
+#endif
+        }
+    }
+}
---
+++ Abusing CSharp/Code/OddsAndEnds/PreprocessorOddities2.cs
@@ -0,0 +1,22 @@
+﻿#define FOO
+using System;
+using System.ComponentModel;
+
+namespace OddsAndEnds
+{
+    [Description("Verbatim string literals")]
+    class PreprocessorOddities2
+    {
+        static void Main()
+        {
+#if FOO
+            string x = @"FOO is defined
+#else
+            string x = @";
+#else
+                         FOO is not defined";
+#endif
+            Console.WriteLine(x);
+        }
+    }
+}
