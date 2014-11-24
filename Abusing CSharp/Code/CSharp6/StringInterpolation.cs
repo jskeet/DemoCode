00509abc301d4d844c96c8520c630ed6fd7953e8
Jon Skeet <skeet@pobox.com>
2014-11-24 20:01:33 +0000

    (And add the file itself... doh.)

---
+++ Abusing CSharp/Code/CSharp6/StringInterpolation.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp6
+{
+    class StringInterpolation
+    {
+        static void Main()
+        {
+            // Braces for multiple statements aren't allowed...
+            // Console.WriteLine("\{{Console.WriteLine("hello"); return "y";}}");
+
+            // But lambda expressions are!
+            Console.WriteLine("\{((Func<string>)(() => {Console.WriteLine("hello"); return "y";}))()}");
+            // Simplified with a method call to avoid the cast...
+            Console.WriteLine("\{F(() => { Console.WriteLine("hello"); return "y"; })}");
+        }
+
+        static string F(Func<string> func)
+        {
+            return func();
+        }
+    }
+}
