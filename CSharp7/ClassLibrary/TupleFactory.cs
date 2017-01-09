0e3a65c13ba2f9293645d8b0f955ab1948d732c2
Jon Skeet <skeet@pobox.com>
2017-01-09 13:14:03 +0000

    wip

--- CSharp7/ClassLibrary/TupleFactory.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ClassLibrary
-{
-    public static class TupleFactory
-    {
-        public static (int x, int y) GetPosition() => (5, 3);
-
-        public static (int dx, int dy) GetVelocity() => (1, 2);
-    }
-}
