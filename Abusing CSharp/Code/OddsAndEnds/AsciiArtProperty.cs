7ea1edef28a622effd77864e01cfb9fb3506fdc6
Jon Skeet <skeet@pobox.com>
2017-01-09 13:03:44 +0000

    wip

--- Abusing CSharp/Code/OddsAndEnds/AsciiArtProperty.cs
+++ Abusing CSharp/Code/OddsAndEnds/AsciiArtProperty.cs
@@ -1,9 +1,4 @@
 ﻿// Copyright 2016 Jon Skeet. All rights reserved. Use of this source code is governed by the Apache License 2.0, as found in the LICENSE.txt file.
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace OddsAndEnds
 {
