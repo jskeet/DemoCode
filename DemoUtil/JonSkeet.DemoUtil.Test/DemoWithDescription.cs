7e5969c3b80be3e2268b5de2f115decda993e89f
Jon Skeet <skeet@pobox.com>
2017-04-06 13:18:13 +0100

    First pass at a standalone demo util package
    This code was in MiscUtil, but it's useful in a smaller package
    which supports .NET Core easily.

---
+++ DemoUtil/JonSkeet.DemoUtil.Test/DemoWithDescription.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace JonSkeet.DemoUtil.Test
+{
+    [Description("This is a description")]
+    class DemoWithDescription
+    {
+        static void Main()
+        {
+            Console.WriteLine("This demo has a description");
+        }
+    }
+}
---
+++ DemoUtil/JonSkeet.DemoUtil.Test/DemoWithoutDescription.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JonSkeet.DemoUtil.Test
+{
+    class DemoWithoutDescription
+    {
+        static void Main()
+        {
+            Console.WriteLine("This demo has no description");
+        }
+    }
+}
