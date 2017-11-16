8e0367a7bd1506a20c5de03c2ef653dc385ab083
Jon Skeet <skeet@pobox.com>
2017-11-16 15:48:34 +0000

    Add demo async main

---
+++ DemoUtil/JonSkeet.DemoUtil.Test/DemoWithAsyncMain.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace JonSkeet.DemoUtil.Test
+{
+    class DemoWithAsyncMain
+    {
+        static async Task Main()
+        {
+            Console.WriteLine("Before delay");
+            await Task.Delay(1000);
+            Console.WriteLine("After delay");
+        }
+    }
+}
