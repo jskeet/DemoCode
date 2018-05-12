5eb702c3ff4cbc94abb0f998a7dfcf43fbc31c03
Jon Skeet <skeet@pobox.com>
2018-05-12 16:23:00 +0100

    More awkward scenarios

---
+++ CSharp8/Nullability/Demos/AwkwardInterface.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demos
+{
+    // Should this be string or string? ?
+    class AwkwardInterface : IEqualityComparer<string?>
+    {
+        static void Main()
+        {
+            var comparer = new AwkwardInterface();
+            // This should work
+            comparer.Equals(null, null);
+            // This should warn - it's going to throw
+            comparer.GetHashCode(null);
+        }
+
+        public bool Equals(string? x, string? y)
+        {
+            if (x == null && y == null)
+            {
+                return true;
+            }
+
+            if (x == null || y == null)
+            {
+                return false;
+            }
+            // Bizarre, but hey...
+            return x.Length == y.Length;
+        }
+
+        public int GetHashCode(string? obj)
+        {
+            // That's a bit weird...
+            if (obj == null)
+            {
+                throw new ArgumentNullException();
+            }
+            return obj.Length;
+        }
+    }
+}
