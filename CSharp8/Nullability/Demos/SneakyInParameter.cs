6efc53955a4d85d858c7dbb5dab067348fe37b95
Jon Skeet <skeet@pobox.com>
2018-05-12 16:02:37 +0100

    Add sneaky in parameter example

---
+++ CSharp8/Nullability/Demos/SneakyInParameter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Demos
+{
+    class SneakyInParameter
+    {
+        static void Main()
+        {
+            string? text = "Not null";
+            Action action = () => text = null;
+            // Boom!
+            int length = LengthIfPossible(text, action);
+        }
+
+        static int LengthIfPossible(in string? text, Action action)
+        {
+            if (text == null)
+            {
+                return -1;
+            }
+            action();
+            // We've checked whether text is null or not, right?
+            // So if we're warning-free, all should be well...
+            return text.Length;
+        }
+    }
+}
