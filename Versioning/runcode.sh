a32b1f0845ba3e7a8cb425a028225fc41d750d1d
Jon Skeet <skeet@pobox.com>
2018-08-13 12:27:40 +0100

    More examples

--- Versioning/runcode.sh
+++ Versioning/runcode.sh
@@ -7,7 +7,10 @@ cd $1
 csc -nologo -langVersion:latest -target:library -out:Library.dll Before.cs
 csc -nologo -langVersion:latest -target:exe -out:Client.exe -r:Library.dll Client.cs
 
+# It's okay if this fails, e.g. due to an expected exception.
+set +e
 ./Client.exe > before-result.txt 2>&1
+set -e
 
 csc -nologo -target:library -out:Library.dll -langVersion:latest After.cs
 
@@ -18,7 +21,7 @@ set +e
 # It's okay if compilation fails; only try to run if it succeeds
 csc -nologo -langVersion:latest -target:exe -out:Client.exe -r:Library.dll Client.cs \
   > after-result-2.txt 2>&1 \
-  && ./Client.exe 2>&1 > after-result-2.txt
+  && ./Client.exe > after-result-2.txt 2>&1
 
 # Always succeed even if the previous command failed
 exit 0
