31c45c04dfc6a89dac15ed2474cde5b6727c3822
Jon Skeet <skeet@pobox.com>
2019-06-29 10:20:55 +0100

    First diamond dependency

---
+++ VersioningBlogPost/Diamond/App/Program.cs
@@ -0,0 +1,15 @@
+﻿using Lib1;
+using NodaTime;
+using System;
+
+namespace App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Class1.GetFixedDate());
+            Console.WriteLine(Class1.GetNow(SystemClock.Instance));
+        }
+    }
+}
