c64e0df51f53241fc4e41d5b78dc90395c999d14
Jon Skeet <skeet@pobox.com>
2019-06-29 10:20:55 +0100

    First diamond dependency

---
+++ VersioningBlogPost/Diamond/Lib1/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using NodaTime;
+
+namespace Lib1
+{
+    public class Class1
+    {
+        public static LocalDate GetFixedDate() =>
+            new LocalDate(2019, 6, 29);
+
+        public static Instant GetNow(IClock clock) =>
+            clock.Now;
+    }
+}
