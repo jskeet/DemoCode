bf723de73ec742e23eabf9870007cba59dd871bc
Jon Skeet <skeet@pobox.com>
2019-07-29 07:09:30 +0100

    Introduce module detection via midi

---
+++ Drums/VDrumExplorer.Midi/ManufacturerId.cs
@@ -0,0 +1,18 @@
+﻿namespace VDrumExplorer.Midi
+{
+    /// <summary>
+    /// Very partial list of manufacturer IDs.
+    /// </summary>
+    public enum ManufacturerId
+    {
+        Moog = 0x04,
+        Apple = 0x11,
+        Cakewalk = 0x121,
+        Kawai = 0x40,
+        Roland = 0x41,
+        Korg = 0x42,
+        Yamaha = 0x43,
+        Casio = 0x44,
+        Akai = 0x47,
+    }
+}
