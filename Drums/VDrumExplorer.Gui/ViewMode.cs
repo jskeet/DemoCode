d2ef820ccedcf2b44ccb2a94ae8ebecf8f9e9419
Jon Skeet <skeet@pobox.com>
2019-08-07 19:08:02 +0100

    Refactor view mode handling
    Also show conditional fields in physical mode

---
+++ Drums/VDrumExplorer.Gui/ViewMode.cs
@@ -0,0 +1,12 @@
+﻿// Copyright 2019 Jon Skeet. All rights reserved.
+// Use of this source code is governed by the Apache License 2.0,
+// as found in the LICENSE.txt file.
+
+namespace VDrumExplorer.Gui
+{
+    internal enum ViewMode
+    {
+        Logical = 0,
+        Physical = 1
+    }
+}
