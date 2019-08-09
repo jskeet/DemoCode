e950e3e00f16f6909e4200b7ed3a28d57ed716c0
Jon Skeet <skeet@pobox.com>
2019-08-09 20:00:13 +0100

    Start of WPF conversion

---
+++ Drums/VDrumExplorer.Wpf/App.xaml.cs
@@ -0,0 +1,18 @@
+﻿using Sanford.Multimedia.Midi;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace VDrumExplorer.Wpf
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
