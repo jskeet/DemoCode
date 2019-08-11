28b53a71b45e8456e7b45c4a44ea66fe7adeb8a7
Jon Skeet <skeet@pobox.com>
2019-08-11 15:20:30 +0100

    Add event to ModuleData for data changing

---
+++ Drums/VDrumExplorer.Data/ModuleDataChangedEventArgs.cs
@@ -0,0 +1,17 @@
+﻿// Copyright 2019 Jon Skeet. All rights reserved.
+// Use of this source code is governed by the Apache License 2.0,
+// as found in the LICENSE.txt file.
+
+using System;
+
+namespace VDrumExplorer.Data
+{
+    public class ModuleDataChangedEventArgs : EventArgs
+    {
+        public DataSegment ChangedSegment { get; }
+
+        // TODO: Options for data being removed?
+        public ModuleDataChangedEventArgs(DataSegment changedSegment) =>
+            ChangedSegment = changedSegment;
+    }
+}
