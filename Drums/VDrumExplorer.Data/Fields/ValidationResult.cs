49025cdd161e8ccc12155b91b2787b5902650777
Jon Skeet <skeet@pobox.com>
2019-08-22 22:33:37 +0100

    Change Module.Valdiate to return a result instead of throwing

---
+++ Drums/VDrumExplorer.Data/Fields/ValidationResult.cs
@@ -0,0 +1,17 @@
+﻿// Copyright 2019 Jon Skeet. All rights reserved.
+// Use of this source code is governed by the Apache License 2.0,
+// as found in the LICENSE.txt file.
+
+using System.Collections.Generic;
+
+namespace VDrumExplorer.Data.Fields
+{
+    public sealed class ValidationResult
+    {
+        public int TotalFields { get; }
+        public IReadOnlyList<ValidationError> Errors { get; }
+
+        public ValidationResult(int totalFields, IReadOnlyList<ValidationError> errors) =>
+            (TotalFields, Errors) = (totalFields, errors);
+    }
+}
