e3722d62c7f133dbf0eb8de194172d2fb12f0a99
Jon Skeet <skeet@pobox.com>
2019-08-24 17:54:34 +0100

    First part of using protos for data files
    The aim is to make the file format more easily extensible.
    Ideally, only code within the Proto namespace will use the protos at all.

---
+++ Drums/VDrumExplorer.Data/Proto/ModuleIdentifier.cs
@@ -0,0 +1,21 @@
+﻿// Copyright 2019 Jon Skeet. All rights reserved.
+// Use of this source code is governed by the Apache License 2.0,
+// as found in the LICENSE.txt file.
+
+namespace VDrumExplorer.Data.Proto
+{
+    internal partial class ModuleIdentifier
+    {
+        internal Data.ModuleIdentifier ToModel() =>
+            new Data.ModuleIdentifier(Name, ModelId, FamilyCode, FamilyNumberCode);
+        
+        internal static ModuleIdentifier FromModel(Data.ModuleIdentifier id) =>
+            new ModuleIdentifier
+            {
+                Name = id.Name,
+                ModelId = id.ModelId,
+                FamilyCode = id.FamilyCode,
+                FamilyNumberCode = id.FamilyNumberCode
+            };
+    }
+}
