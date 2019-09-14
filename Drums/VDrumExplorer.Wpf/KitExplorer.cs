927a1159ea0e93bcacc7314bdb9b8852d36fb4c8
Jon Skeet <skeet@pobox.com>
2019-09-14 12:50:08 +0100

    Collapse KitExplorer and ModuleExplorer into a single base class
    DataExplorer contains all the UI for both KitExplorer and ModuleExplorer, which are now subclasses.
    The subclasses each hide bits of UI they don't use, and override behavior that is kit/module-specific

---
+++ Drums/VDrumExplorer.Wpf/KitExplorer.cs
@@ -0,0 +1,79 @@
+﻿// Copyright 2019 Jon Skeet. All rights reserved.
+// Use of this source code is governed by the Apache License 2.0,
+// as found in the LICENSE.txt file.
+
+using Microsoft.Win32;
+using System;
+using System.Globalization;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows;
+using VDrumExplorer.Data;
+using VDrumExplorer.Midi;
+
+namespace VDrumExplorer.Wpf
+{
+    public class KitExplorer : DataExplorer
+    {
+        private readonly Kit kit;
+
+        internal KitExplorer(ILogger logger, Kit kit, SysExClient midiClient)
+            : base(logger, kit.Schema, kit.Data, kit.KitRoot, midiClient)
+        {
+            this.kit = kit;
+            openKitButton.Visibility = Visibility.Collapsed;
+            copyToDeviceButton.Content = "Copy kit to device";
+            Title = $"Kit explorer: {Schema.Identifier.Name}";
+        }
+
+        protected override void SaveFile(object sender, EventArgs e)
+        {
+            var dialog = new SaveFileDialog { Filter = "VDrum Explorer kit files|*.vkit" };
+            var result = dialog.ShowDialog();
+            if (result != true)
+            {
+                return;
+            }
+            using (var stream = File.OpenWrite(dialog.FileName))
+            {
+                kit.Save(stream);
+            }
+        }
+
+        protected override async void CopyToDevice(object sender, RoutedEventArgs e)
+        {
+            if (!int.TryParse(copyToDeviceKitNumber.Text, NumberStyles.None, CultureInfo.InvariantCulture, out int kitToCopyTo))
+            {
+                Logger.Log("Invalid kit number");
+                return;
+            }
+
+            if (!Schema.KitRoots.TryGetValue(kitToCopyTo, out var targetKitRoot))
+            {
+                Logger.Log("Unknown kit number");
+                return;
+            }
+
+            // It's simplest to clone our root node into a new ModuleData at the right place,
+            // then send all those segments. It does involve copying the data in memory
+            // twice, but that's much quicker than sending it to the kit anyway.
+            var clonedData = RootNode.Context.CloneData(Data, targetKitRoot.Context.Address);
+            var segments = clonedData.GetSegments();
+            midiPanel.IsEnabled = false;
+            try
+            {
+                Logger.Log($"Writing {segments.Count} segments to the device.");
+                foreach (var segment in segments)
+                {
+                    MidiClient.SendData(segment.Start.Value, segment.CopyData());
+                    await Task.Delay(40);
+                }
+                Logger.Log($"Finished writing segments to the device.");
+            }
+            finally
+            {
+                midiPanel.IsEnabled = true;
+            }
+        }
+    }
+}
---
+++ Drums/VDrumExplorer.Wpf/ModuleExplorer.cs
@@ -0,0 +1,127 @@
+﻿// Copyright 2019 Jon Skeet. All rights reserved.
+// Use of this source code is governed by the Apache License 2.0,
+// as found in the LICENSE.txt file.
+
+using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using VDrumExplorer.Data;
+using VDrumExplorer.Data.Layout;
+using VDrumExplorer.Midi;
+
+namespace VDrumExplorer.Wpf
+{
+    public class ModuleExplorer : DataExplorer
+    {
+        private readonly Module module;
+
+        internal ModuleExplorer(ILogger logger, Module module, SysExClient midiClient)
+            : base(logger, module.Schema, module.Data, module.Schema.LogicalRoot, midiClient)
+        {
+            this.module = module;
+            Title = $"Module explorer: {Schema.Identifier.Name}";
+            kitNumberLabel.Visibility = Visibility.Collapsed;
+            copyToDeviceKitNumber.Visibility = Visibility.Collapsed;
+        }
+
+        protected override void SaveFile(object sender, EventArgs e)
+        {
+            var dialog = new SaveFileDialog { Filter = "VDrum Explorer module files|*.vdrum" };
+            var result = dialog.ShowDialog();
+            if (result != true)
+            {
+                return;
+            }
+            using (var stream = File.OpenWrite(dialog.FileName))
+            {
+                module.Save(stream);
+            }
+        }
+
+        protected override void OpenKitInKitExplorer(object sender, RoutedEventArgs e)
+        {
+            var node = (VisualTreeNode) detailsPanel.Tag;
+            var kitNode = FindKitNode(node);
+
+            // We try to protect against this in terms of enabling/disabling the button, but
+            // let's be cautious anyway.
+            if (kitNode == null)
+            {
+                return;
+            }
+
+            // We clone the data from kitNode downwards, but relocating it as if it were the first kit.
+            var firstKitNode = Schema.KitRoots[1];
+            var clonedData = kitNode.Context.CloneData(Data, firstKitNode.Context.Address);
+            var kit = new Kit(Schema, clonedData);
+            new KitExplorer(Logger, kit, MidiClient).Show();
+        }
+
+        private VisualTreeNode FindKitNode(VisualTreeNode currentNode)
+        {
+            while (currentNode != null)
+            {
+                if (currentNode.KitNumber != null)
+                {
+                    return currentNode;
+                }
+                currentNode = currentNode.Parent;
+            }
+            return null;
+        }
+
+        protected override void LoadDetailsPage()
+        {
+            base.LoadDetailsPage();
+            var node = (VisualTreeNode) detailsPanel.Tag;
+            openKitButton.IsEnabled = FindKitNode(node) is object;
+        }
+
+        protected override async void CopyToDevice(object sender, RoutedEventArgs e)
+        {
+            var node = (VisualTreeNode) detailsPanel.Tag;
+            if (node == null)
+            {
+                return;
+            }
+
+            // Find all the segments we need.
+            // We assume that each item of data is reflected in the details as a container
+            // rather than a formatted description either in the details page or in the tree.
+            // (Only containers have *editable* data anyway.)
+            // We can't just save "all containers under the fixed container" as instruments
+            // use several peer containers rather than "everything under one node".
+            var segments = node.DescendantNodesAndSelf()
+                .SelectMany(GetSegments)
+                .Distinct()
+                .OrderBy(segment => segment.Start)
+                .ToList();
+            midiPanel.IsEnabled = false;
+            try
+            {
+                Logger.Log($"Writing {segments.Count} segments to the device.");
+                foreach (var segment in segments)
+                {
+                    MidiClient.SendData(segment.Start.Value, segment.CopyData());
+                    await Task.Delay(40);
+                }
+                Logger.Log($"Finished writing segments to the device.");
+            }
+            finally
+            {
+                midiPanel.IsEnabled = true;
+            }
+
+            IEnumerable<DataSegment> GetSegments(VisualTreeNode treeNode) =>
+                treeNode.Details
+                    .Where(d => d.Container is object)
+                    .Select(d => d.FixContainer(treeNode.Context))
+                    .Where(fc => fc.Container.Loadable)
+                    .Select(fc => Data.GetSegment(fc.Address));
+        }
+    }
+}
