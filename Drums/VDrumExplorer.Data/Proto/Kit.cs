e2fd7989a2569a56f01fbb33169516a934c7052c
Jon Skeet <skeet@pobox.com>
2019-09-14 11:03:57 +0100

    Introduce a Kit model and proto IO to handle it

---
+++ Drums/VDrumExplorer.Data/Proto/Kit.cs
@@ -0,0 +1,21 @@
+﻿// Copyright 2019 Jon Skeet. All rights reserved.
+// Use of this source code is governed by the Apache License 2.0,
+// as found in the LICENSE.txt file.
+
+using System.Linq;
+
+namespace VDrumExplorer.Data.Proto
+{
+    internal partial class Kit
+    {
+        internal Data.Kit ToModel() =>
+            new Data.Kit(Identifier.GetSchema(), DataSegment.LoadData(Segments));
+
+        internal static Kit FromModel(Data.Kit kit) =>
+            new Kit
+            {
+                Identifier = ModuleIdentifier.FromModel(kit.Schema.Identifier),
+                Segments = { kit.Data.GetSegments().Select(DataSegment.FromModel) }
+            };
+    }
+}
