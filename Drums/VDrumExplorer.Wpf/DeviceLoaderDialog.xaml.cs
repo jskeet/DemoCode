43020113e8aa58b17b6af72c6f544d25843b9d45
Jon Skeet <skeet@pobox.com>
2019-09-14 11:06:08 +0100

    Modify the device loader dialog to handle loading a single container (and descendants)

--- Drums/VDrumExplorer.Wpf/DeviceLoaderDialog.xaml.cs
+++ Drums/VDrumExplorer.Wpf/DeviceLoaderDialog.xaml.cs
@@ -23,9 +23,8 @@ namespace VDrumExplorer.Wpf
         private readonly SysExClient client;
         private readonly ModuleSchema schema;
         private readonly CancellationTokenSource cancellationTokenSource;
-        private readonly ModuleData data;
         
-        public Module Module { get; private set; }
+        public ModuleData Data { get; private set; }
 
         public DeviceLoaderDialog()
         {
@@ -37,18 +36,17 @@ namespace VDrumExplorer.Wpf
             this.logger = logger;
             this.client = client;
             this.schema = schema;
-            data = new ModuleData();
             cancellationTokenSource = new CancellationTokenSource(TimeSpan.FromMinutes(10));
-            Loaded += LoadDeviceData;
         }
         
-        private async void LoadDeviceData(object sender, RoutedEventArgs e)
+        internal async void LoadDeviceData(FixedContainer root)
         {
+            var data = new ModuleData();
             client.UnconsumedMessageHandler += LogUnconsumedMessage;
             try
             {
                 Stopwatch sw = Stopwatch.StartNew();
-                var containers = schema.Root.AnnotateDescendantsAndSelf().Where(c => c.Container.Loadable).ToList();
+                var containers = root.AnnotateDescendantsAndSelf().Where(c => c.Container.Loadable).ToList();
                 logger.Log($"Loading {containers.Count} containers from device {schema.Identifier.Name}");
                 progress.Maximum = containers.Count;
                 int loaded = 0;
@@ -56,11 +54,11 @@ namespace VDrumExplorer.Wpf
                 {
                     // TODO: Make RequestDataAsync return a segment.
                     label.Content = $"Loading {container.Path}";
-                    await PopulateSegment(container, cancellationTokenSource.Token);
+                    await PopulateSegment(data, container, cancellationTokenSource.Token);
                     loaded++;
                     progress.Value = loaded;
                 }
-                Module = new Module(schema, data);
+                Data = data;
                 DialogResult = true;
                 logger.Log($"Finished loading in {(int) sw.Elapsed.TotalSeconds} seconds");
             }
@@ -82,7 +80,7 @@ namespace VDrumExplorer.Wpf
                 logger.Log($"Unexpected data response. Address: {message.Address:x8}; Length: {message.Length:x8}");
         }
 
-        private async Task PopulateSegment(AnnotatedContainer annotatedContainer, CancellationToken token)
+        private async Task PopulateSegment(ModuleData data, AnnotatedContainer annotatedContainer, CancellationToken token)
         {
             var timerToken = new CancellationTokenSource(TimeSpan.FromSeconds(1)).Token;
             var effectiveToken = CancellationTokenSource.CreateLinkedTokenSource(token, timerToken).Token;
