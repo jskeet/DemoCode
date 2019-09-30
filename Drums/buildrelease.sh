fa6916120b1cf829a38b8e872457789d91daae3e
Jon Skeet <skeet@pobox.com>
2019-09-30 18:29:17 +0100

    First attempt at a Wix installer

--- Drums/buildrelease.sh
+++ Drums/buildrelease.sh
@@ -8,7 +8,12 @@ then
   exit 1
 fi
 
+# Build executables
 dotnet build -c Release -v quiet VDrumExplorer.Wpf
+# Sign executables in-place
+signtool sign -a -fd SHA256 -f $PFX_PATH -p $PFX_PASSWORD -t http://timestamp.comodoca.com/authenticode VDrumExplorer.Wpf/bin/Release/netcoreapp3.0/VDrumExplorer.Wpf.exe
+signtool sign -a -fd SHA256 -f $PFX_PATH -p $PFX_PASSWORD -t http://timestamp.comodoca.com/authenticode VDrumExplorer.Wpf/bin/Release/net472/VDrumExplorer.Wpf.exe
+
 version=$(grep \<Version\> VDrumExplorer.Wpf/VDrumExplorer.Wpf.csproj | sed s/\<[^\>]*\>//g | sed 's/ //g')
 
 rm -rf tmp
@@ -19,7 +24,6 @@ cd tmp
 release_dir=VDrumExplorer-DotNetCore-$version
 mkdir $release_dir
 cp ../VDrumExplorer.Wpf/bin/Release/netcoreapp3.0/* $release_dir
-signtool sign -a -fd SHA256 -f $PFX_PATH -p $PFX_PASSWORD -t http://timestamp.comodoca.com/authenticode $release_dir/VDrumExplorer.Wpf.exe
 cp ../td17.vdrum $release_dir
 cp ../LICENSE* $release_dir
 cp ../README.md $release_dir
@@ -29,10 +33,15 @@ zip -rq VDrumExplorer-DotNetCore-$version.zip $release_dir
 release_dir=VDrumExplorer-Desktop-$version
 mkdir $release_dir
 cp ../VDrumExplorer.Wpf/bin/Release/net472/* $release_dir
-signtool sign -a -fd SHA256 -f $PFX_PATH -p $PFX_PASSWORD -t http://timestamp.comodoca.com/authenticode $release_dir/VDrumExplorer.Wpf.exe
 cp ../td17.vdrum $release_dir
 cp ../LICENSE* $release_dir
 cp ../README.md $release_dir
 zip -rq VDrumExplorer-Desktop-$version.zip $release_dir
+
+# Setup
+msbuild.exe -property:Configuration=Release -verbosity:quiet ../VDrumExplorer.Setup
+cp ../VDrumExplorer.Setup/bin/Release/en-us/VDrumExplorer-Setup.msi .
+# Sign the installer as well
+signtool sign -a -fd SHA256 -f $PFX_PATH -p $PFX_PASSWORD -t http://timestamp.comodoca.com/authenticode VDrumExplorer-Setup.msi
 
 cd ..
