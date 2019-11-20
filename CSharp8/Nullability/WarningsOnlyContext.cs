7ecc0262bbcb29ba8b3bcd214cd7976f78fa2d2f
Jon Skeet <skeet@pobox.com>
2019-11-20 06:51:06 +0000

    Generics and contexts

---
+++ CSharp8/Nullability/WarningsOnlyContext.cs
@@ -0,0 +1,28 @@
+﻿#nullable enable warnings
+#nullable disable annotations
+
+using System;
+
+namespace Nullability
+{
+    class WarningsOnlyContext
+    {
+        static void Main()
+        {
+            Check(null);
+            Check("not null");
+        }
+
+        static void Check(string x)
+        {
+            if (x is null)
+            {
+                Console.WriteLine("x is null");
+            }
+            else
+            {
+                Console.WriteLine($"x is not-null, with length {x.Length}");
+            }
+        }
+    }
+}
