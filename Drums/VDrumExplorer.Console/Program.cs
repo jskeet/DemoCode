f015df68c5b888e583ad71b1f0a238389d878774
Jon Skeet <skeet@pobox.com>
2020-01-27 12:40:30 +0000

    Start of a more general-purpose command line tool

--- Drums/VDrumExplorer.Console/Program.cs
+++ Drums/VDrumExplorer.Console/Program.cs
@@ -2,51 +2,23 @@
 // Use of this source code is governed by the Apache License 2.0,
 // as found in the LICENSE.txt file.
 
-using Commons.Music.Midi;
-using System;
-using System.Linq;
+using System.CommandLine;
 using System.Threading.Tasks;
-using VDrumExplorer.Midi;
 
-namespace VDrumExplorer.ConsoleDemo
+namespace VDrumExplorer.Console
 {
     class Program
     {
-        static async Task Main(string[] args)
+        static Task<int> Main(string[] args)
         {
-            // Just so we can write synchronous code when we want to
-            await Task.Yield();
-
-            try
+            var rootCommand = new RootCommand
             {
-                var inputs = MidiDevices.ListInputDevices();
-                foreach (var input in inputs)
-                {
-                    Console.WriteLine($"Input: {input}");
-                }
-                var outputs = MidiDevices.ListOutputDevices();
-                foreach (var output in outputs)
-                {
-                    Console.WriteLine($"Output: {output}");
-                }
-                var td17Input = inputs.Single(input => input.Name == "2- TD-17");
-                var td17Output = outputs.Single(output => output.Name == "2- TD-17");
-
-                var identities = await MidiDevices.ListDeviceIdentities(td17Input, td17Output, TimeSpan.FromSeconds(0.5));
-                foreach (var identity in identities)
-                {
-                    Console.WriteLine(identity);
-                }
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e);
-            }
-        }
-
-        private static void Input_MessageReceived(object sender, MidiReceivedEventArgs e)
-        {
-            throw new NotImplementedException();
+                Description = "V-Drum Explorer console interface"
+            };
+            rootCommand.AddCommand(ListDevicesCommand.Command);
+            rootCommand.AddCommand(ImportKitCommand.Command);
+            rootCommand.AddCommand(ShowKitCommand.Command);
+            return rootCommand.InvokeAsync(args);
         }
     }
 }
