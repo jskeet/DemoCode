ff0ef654c0e31dc8260fdea5c96baa578514481f
Jon Skeet <skeet@pobox.com>
2020-01-31 07:00:58 +0000

    Implement the JSON formatting in ShowKitCommand

--- Drums/VDrumExplorer.Console/ShowKitCommand.cs
+++ Drums/VDrumExplorer.Console/ShowKitCommand.cs
@@ -2,13 +2,18 @@
 // Use of this source code is governed by the Apache License 2.0,
 // as found in the LICENSE.txt file.
 
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using System;
 using System.CommandLine;
 using System.CommandLine.Invocation;
 using System.CommandLine.IO;
 using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
 using VDrumExplorer.Data;
+using VDrumExplorer.Data.Fields;
+using VDrumExplorer.Data.Layout;
 
 namespace VDrumExplorer.Console
 {
@@ -27,7 +32,7 @@ namespace VDrumExplorer.Console
             return command;
         }
 
-        public async Task<int> InvokeAsync(InvocationContext context)
+        public Task<int> InvokeAsync(InvocationContext context)
         {
             var console = context.Console.Out;
             var file = context.ParseResult.ValueForOption<string>("file");
@@ -43,21 +48,58 @@ namespace VDrumExplorer.Console
             catch (Exception ex)
             {
                 console.WriteLine($"Error loading {file}: {ex}");
-                return 1;
+                return Task.FromResult(1);
             }
 
             if (!(loaded is Kit kit))
             {
                 console.WriteLine($"File did not parse as a kit file");
-                return 1;
+                return Task.FromResult(1);
             }
 
-            // TODO: Write actual code to create a JObject from the data.
-            console.WriteLine($"Kit number: {kit.DefaultKitNumber}");
+            var json = ConvertToJson(kit.KitRoot, kit.Data);
+            console.WriteLine(json.ToString(Formatting.Indented));
+            return Task.FromResult(0);
+        }
 
-            // Just so it's async...
-            await Task.Yield();
-            return 0;
+        private static JObject ConvertToJson(VisualTreeNode container, ModuleData data)
+        {
+            var ret = new JObject();
+            foreach (var child in container.Children)
+            {
+                var description = child.KitOnlyDescription ?? child.Description;
+                ret.Add(description.Format(child.Context, data), ConvertToJson(child, data));
+            }
+            foreach (var detail in container.Details)
+            {
+                ret.Add(detail.Description, ConvertToJson(detail, container.Context, data));
+            }
+            return ret;
+        }
+
+        private static JToken ConvertToJson(VisualTreeDetail detail, FixedContainer context, ModuleData data)
+        {
+            if (detail.DetailDescriptions is object)
+            {
+                var ret = new JArray();
+                foreach (var description in detail.DetailDescriptions)
+                {
+                    ret.Add(description.Format(context, data));
+                }
+                return ret;
+            }
+            else
+            {
+                var ret = new JObject();
+                var container = detail.Container;
+                var fields = container.GetPrimitiveFields(data)
+                    .Where(f => f.IsEnabled(context, data));
+                foreach (var field in fields)
+                {
+                    ret.Add(field.Description, field.GetText(container, data));
+                }
+                return ret;
+            }
         }
     }
 }
