29dbddd495a3e51962208f4ea4a6b493231a21ac
Jon Skeet <skeet@pobox.com>
2020-04-05 16:12:52 +0100

    Start of the new WPF app; this project only contains view logic
    Currently just a schema explorer, so I can learn more about WPF in the relatively straightforward, read-only world of schemas.

---
+++ Drums/VDrumExplorer.Gui/App.xaml.cs
@@ -0,0 +1,24 @@
+﻿// Copyright 2020 Jon Skeet. All rights reserved.
+// Use of this source code is governed by the Apache License 2.0,
+// as found in the LICENSE.txt file.
+
+using System.Windows;
+using VDrumExplorer.Model;
+using VDrumExplorer.ViewModel.LogicalSchema;
+
+namespace VDrumExplorer.Gui
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            var schema = ModuleSchema.KnownSchemas[ModuleIdentifier.TD27].Value;
+            MainWindow = new SchemaExplorer { DataContext = new ModuleSchemaViewModel(schema) };
+            MainWindow.Show();
+        }
+    }
+}
---
+++ Drums/VDrumExplorer.Gui/SchemaExplorer.xaml.cs
@@ -0,0 +1,25 @@
+﻿// Copyright 2020 Jon Skeet. All rights reserved.
+// Use of this source code is governed by the Apache License 2.0,
+// as found in the LICENSE.txt file.
+
+using System.Windows;
+using VDrumExplorer.ViewModel.LogicalSchema;
+
+namespace VDrumExplorer.Gui
+{
+    /// <summary>
+    /// Interaction logic for SchemaExplorer.xaml
+    /// </summary>
+    public partial class SchemaExplorer : Window
+    {
+        public SchemaExplorer()
+        {
+            InitializeComponent();
+        }
+
+        private void TreeView_SelectedItemChanged(object sender, RoutedPropertyChangedEventArgs<object> e)
+        {
+            ((ModuleSchemaViewModel) DataContext).SelectedNode = (TreeNodeViewModel) treeView.SelectedItem;
+        }
+    }
+}
