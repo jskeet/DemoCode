0c541e43cdc72f9eef2961b71443ed669f80d964
Jon Skeet <skeet@pobox.com>
2020-04-21 06:52:56 +0100

    Add benchmarks project to measure performance

---
+++ Drums/VDrumExplorer.Benchmarks/Program.cs
@@ -0,0 +1,17 @@
+﻿// Copyright 2020 Jon Skeet. All rights reserved.
+// Use of this source code is governed by the Apache License 2.0,
+// as found in the LICENSE.txt file.
+
+using BenchmarkDotNet.Running;
+using System;
+
+namespace VDrumExplorer.Benchmarks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BenchmarkRunner.Run(typeof(Program).Assembly);
+        }
+    }
+}
