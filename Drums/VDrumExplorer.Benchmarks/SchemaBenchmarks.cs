209687be127af98cda0549c63adffa20b7ef8bbe
Jon Skeet <skeet@pobox.com>
2020-04-21 06:52:56 +0100

    Add benchmarks project to measure performance

---
+++ Drums/VDrumExplorer.Benchmarks/SchemaBenchmarks.cs
@@ -0,0 +1,17 @@
+﻿// Copyright 2020 Jon Skeet. All rights reserved.
+// Use of this source code is governed by the Apache License 2.0,
+// as found in the LICENSE.txt file.
+
+using BenchmarkDotNet.Attributes;
+using VDrumExplorer.Model;
+
+namespace VDrumExplorer.Benchmarks
+{
+    [MemoryDiagnoser]
+    public class SchemaBenchmarks
+    {
+        [Benchmark]
+        public ModuleSchema LoadTD27Schema() =>
+            ModuleSchema.FromAssemblyResources(typeof(ModuleSchema).Assembly, "TD27", "TD27.json");
+    }
+}
