df963594ac47c0285423d42a25e9dbfad0415028
Jon Skeet <skeet@pobox.com>
2020-05-10 06:30:31 +0100

    Add kit import/export functionality to Module/Kit models

--- Drums/VDrumExplorer.Model/Data/ModuleDataSnapshot.cs
+++ Drums/VDrumExplorer.Model/Data/ModuleDataSnapshot.cs
@@ -2,7 +2,9 @@
 // Use of this source code is governed by the Apache License 2.0,
 // as found in the LICENSE.txt file.
 
+using System;
 using System.Collections.Generic;
+using VDrumExplorer.Model.Schema.Logical;
 
 namespace VDrumExplorer.Model.Data
 {
@@ -13,22 +15,45 @@ namespace VDrumExplorer.Model.Data
     /// </summary>
     public sealed class ModuleDataSnapshot
     {
-        private readonly SortedDictionary<ModuleAddress, DataSegment> segments;
+        private readonly SortedDictionary<ModuleAddress, DataSegment> segmentsByAddress;
 
-        public IEnumerable<DataSegment> Segments => segments.Values;
+        public IEnumerable<DataSegment> Segments => segmentsByAddress.Values;
 
         public ModuleDataSnapshot()        
         {
-            segments = new SortedDictionary<ModuleAddress, DataSegment>();
+            segmentsByAddress = new SortedDictionary<ModuleAddress, DataSegment>();
         }
 
-        public int SegmentCount => segments.Count;
+        public int SegmentCount => segmentsByAddress.Count;
 
-        internal DataSegment this[ModuleAddress address] => segments[address];
+        internal DataSegment this[ModuleAddress address] => segmentsByAddress[address];
 
         public bool TryGetSegment(ModuleAddress address, out DataSegment segment) =>
-            segments.TryGetValue(address, out segment);
+            segmentsByAddress.TryGetValue(address, out segment);
 
-        public void Add(DataSegment segment) => segments.Add(segment.Address, segment);
+        public void Add(DataSegment segment) => segmentsByAddress.Add(segment.Address, segment);
+
+        /// <summary>
+        /// Creates a new snapshot with all the segments in this one, but relocated
+        /// such that the data is <paramref name="from"/> is moved to <paramref name="to"/>
+        /// (and all other data is offset by the same amount).
+        /// </summary>
+        internal ModuleDataSnapshot Relocated(ModuleAddress from, ModuleAddress to)
+        {
+            var offset = to.LogicalValue - from.LogicalValue;
+            var snapshot = new ModuleDataSnapshot();
+            foreach (var segment in Segments)
+            {
+                snapshot.Add(segment.WithAddress(segment.Address.PlusLogicalOffset(offset)));
+            }
+            return snapshot;
+        }
+
+        /// <summary>
+        /// Convenience method to call <see cref="Relocated(ModuleAddress, ModuleAddress)"/>
+        /// with addresses taken from logical tree nodes.
+        /// </summary>
+        internal ModuleDataSnapshot Relocated(TreeNode from, TreeNode to) =>
+            Relocated(from.Container.Address, to.Container.Address);
     }
 }
