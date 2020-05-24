52ededb390ff36f873c85e869b4007af235ad59e
Jon Skeet <skeet@pobox.com>
2020-05-24 16:37:37 +0100

    Fix new warning around nullability

--- Drums/VDrumExplorer.Model/Schema/Json/HexInt32Converter.cs
+++ Drums/VDrumExplorer.Model/Schema/Json/HexInt32Converter.cs
@@ -10,10 +10,10 @@ namespace VDrumExplorer.Model.Schema.Json
 {
     internal class HexInt32Converter : JsonConverter<HexInt32>
     {
-        public override void WriteJson(JsonWriter writer, HexInt32 value, JsonSerializer serializer) =>
-            writer.WriteValue(value.ToString());
+        public override void WriteJson(JsonWriter writer, HexInt32? value, JsonSerializer serializer) =>
+            writer.WriteValue(value?.ToString());
 
-        public override HexInt32 ReadJson(JsonReader reader, Type objectType, HexInt32 existingValue, bool hasExistingValue, JsonSerializer serializer) =>
+        public override HexInt32 ReadJson(JsonReader reader, Type objectType, HexInt32? existingValue, bool hasExistingValue, JsonSerializer serializer) =>
             HexInt32.Parse(Preconditions.AssertNotNull((string?) reader.Value));
     }
 }
