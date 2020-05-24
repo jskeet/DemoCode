da5079b7b273262c2c9ee6d927a68dfdab560e1f
Jon Skeet <skeet@pobox.com>
2020-05-24 16:40:39 +0100

    Introduce DeviceController to centralize data transfer operations etc
    (The RolandMidiClient class in the Midi package doesn't know about data segments etc - it's lower level than this.)

---
+++ Drums/VDrumExplorer.Model/Device/IDeviceController.cs
@@ -0,0 +1,34 @@
+﻿// Copyright 2020 Jon Skeet. All rights reserved.
+// Use of this source code is governed by the Apache License 2.0,
+// as found in the LICENSE.txt file.
+
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace VDrumExplorer.Model.Device
+{
+    public interface IDeviceController : IDisposable
+    {
+        ModuleSchema Schema { get; }
+        string InputName { get; }
+        string OutputName { get; }
+
+        /// <summary>
+        /// Returns the 1-based current kit number on the device.
+        /// </summary>
+        Task<int> GetCurrentKitAsync(CancellationToken cancellationToken);
+
+        /// <summary>
+        /// Sets the current kit number on the device.
+        /// </summary>
+        /// <param name="kit">1-based kit number</param>
+        Task SetCurrentKitAsync(int kit, CancellationToken cancellationToken);
+
+        Task<Module> LoadModuleAsync(IProgress<TransferProgress> progressHandler, CancellationToken cancellationToken);
+        Task<Kit> LoadKitAsync(int kit, IProgress<TransferProgress> progressHandler, CancellationToken cancellationToken);
+
+        Task PlayNoteAsync(int channel, int note, int attack);
+        Task SilenceAsync(int channel);
+    }
+}
---
+++ Drums/VDrumExplorer.Model/Device/TransferProgress.cs
@@ -0,0 +1,19 @@
+﻿// Copyright 2020 Jon Skeet. All rights reserved.
+// Use of this source code is governed by the Apache License 2.0,
+// as found in the LICENSE.txt file.
+
+namespace VDrumExplorer.Model.Device
+{
+    /// <summary>
+    /// Indication of the progress when transferring data.
+    /// </summary>
+    public sealed class TransferProgress
+    {
+        public int Completed { get; }
+        public int Total { get; }
+        public string Current { get; }
+
+        public TransferProgress(int completed, int total, string current) =>
+            (Completed, Total, Current) = (completed, total, current);
+    }
+}
