ebbc9c3bd36a71dbd97260a27c47e03fad48724e
Jon Skeet <skeet@pobox.com>
2020-05-31 15:56:06 +0100

    Fix nullability warning

--- Drums/VDrumExplorer.ViewModel/Logging/LogViewModel.cs
+++ Drums/VDrumExplorer.ViewModel/Logging/LogViewModel.cs
@@ -64,7 +64,7 @@ namespace VDrumExplorer.ViewModel.Logging
 
             public bool IsEnabled(LogLevel logLevel) => true;
 
-            public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception? exception, Func<TState, Exception, string> formatter)
+            public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception? exception, Func<TState, Exception?, string> formatter)
             {
                 var message = formatter(state, exception);
                 var entry = new LogEntry(clock.GetCurrentInstant(), message, logLevel, exception);
