f6e2620f2d92e121ac5a8f205ee2113b6cf2b789
Jon Skeet <skeet@pobox.com>
2020-05-31 16:28:42 +0100

    Downgrade Model and Utility projects to netstandard2.0
    This should allow use to build a net472 version of the app

--- Drums/VDrumExplorer.Model/Data/Fields/StringDataField.cs
+++ Drums/VDrumExplorer.Model/Data/Fields/StringDataField.cs
@@ -6,6 +6,7 @@ using System;
 using System.Linq;
 using System.Text;
 using VDrumExplorer.Model.Schema.Fields;
+using VDrumExplorer.Utility;
 
 namespace VDrumExplorer.Model.Data.Fields
 {
---
+++ Drums/VDrumExplorer.Utility/CompatibilityExtensions.cs
@@ -0,0 +1,44 @@
+﻿// Copyright 2019 Jon Skeet. All rights reserved.
+// Use of this source code is governed by the Apache License 2.0,
+// as found in the LICENSE.txt file.
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VDrumExplorer.Utility
+{
+    /// <summary>
+    /// Extension methods that are in .NET Standard 2.1, but which we can't use yet...
+    /// </summary>
+    public static class CompatibilityExtensions
+    {
+        public static TValue GetValueOrDefault<TKey, TValue>(this IReadOnlyDictionary<TKey, TValue> dictionary, TKey key) =>
+            GetValueOrDefault(dictionary, key, default!);
+
+        public static TValue GetValueOrDefault<TKey, TValue>(this IReadOnlyDictionary<TKey, TValue> dictionary, TKey key, TValue defaultValue) =>
+            dictionary.TryGetValue(key, out var value) ? value : defaultValue;
+
+        public static void Deconstruct<TKey, TValue>(this KeyValuePair<TKey, TValue> pair, out TKey key, out TValue value) =>
+            (key, value) = (pair.Key, pair.Value);
+
+        public static unsafe string GetString(this Encoding encoding, Span<byte> bytes)
+        {
+            fixed (byte* ptr = bytes)
+            {
+                return encoding.GetString(ptr, bytes.Length);
+            }
+        }
+
+        public static unsafe void GetBytes(this Encoding encoding, ReadOnlySpan<char> text, Span<byte> bytes)
+        {
+            fixed (byte* outputPtr = bytes)
+            {
+                fixed (char* inputPtr = text)
+                {
+                    encoding.GetBytes(inputPtr, text.Length, outputPtr, bytes.Length);
+                }
+            }
+        }
+    }
+}
