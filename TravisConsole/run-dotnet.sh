75a32b2e9c6b4b0854c400215459c443944e9e2b
Jon Skeet <skeet@pobox.com>
2020-07-23 09:13:33 +0100

    Run using --no-build and the direct dll

--- TravisConsole/run-dotnet.sh
+++ TravisConsole/run-dotnet.sh
@@ -11,3 +11,11 @@ echo "After dotnet run (first)"
 echo "Before dotnet run (second)"
 dotnet run -- $1
 echo "After dotnet run (second)"
+
+echo "Before dotnet run --no-build"
+dotnet run --no-build -- $1
+echo "After dotnet run --no-build "
+
+echo "Before dotnet dll"
+dotnet bin/Debug/netcoreapp3.1/TravisConsole.dll $1
+echo "After dotnet dll"
