799c403ebc1e22181f164cbe4da58f914c39edaa
Jon Skeet <skeet@pobox.com>
2020-07-25 17:16:54 +0100

    Minor tidy-up

--- Drums/VDrumExplorer.Model/Schema/Fields/FieldBase.cs
+++ Drums/VDrumExplorer.Model/Schema/Fields/FieldBase.cs
@@ -2,7 +2,6 @@
 // Use of this source code is governed by the Apache License 2.0,
 // as found in the LICENSE.txt file.
 
-using System;
 using VDrumExplorer.Model.Schema.Physical;
 
 namespace VDrumExplorer.Model.Schema.Fields
