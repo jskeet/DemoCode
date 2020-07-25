67ac274221b9454e14bf0b0dd019d56f99947df1
Jon Skeet <skeet@pobox.com>
2020-07-25 17:23:01 +0100

    Store common parameters in an object for NumericField
    This should shrink the size of NumericField significantly.
    
    We could potentially do this for all types, maybe even having a single field with "the common parameters" and casting. But it's most important here.

--- Drums/VDrumExplorer.Model/Schema/Fields/NumericField.cs
+++ Drums/VDrumExplorer.Model/Schema/Fields/NumericField.cs
@@ -11,24 +11,44 @@ namespace VDrumExplorer.Model.Schema.Fields
     /// </summary>
     public sealed class NumericField : NumericFieldBase
     {
+        private readonly NumericFieldParameters numericFieldParameters;
+
         /// <summary>
         /// Some fields have a single "special" value (e.g. "Off", or "-INF").
         /// If this property is non-null, it provides the raw value and appropriate text.
         /// </summary>
-        public (int value, string text)? CustomValueFormatting { get; }
-        public string? OffLabel { get; }
-        public int? Divisor { get; }
-        public int? Multiplier { get; }
-        public int? ValueOffset { get; }
-        public string? Suffix { get; }
+        public (int value, string text)? CustomValueFormatting => numericFieldParameters.CustomValueFormatting;
+        public string? OffLabel => numericFieldParameters.OffLabel;
+        public int? Divisor => numericFieldParameters.Divisor;
+        public int? Multiplier => numericFieldParameters.Multiplier;
+        public int? ValueOffset => numericFieldParameters.ValueOffset;
+        public string? Suffix => numericFieldParameters.Suffix;
+
+        private NumericField(FieldContainer? parent, FieldParameters common, int min, int max, int @default, NumericFieldParameters numericFieldParameters)
+            : base(parent, common, min, max, @default) =>
+            this.numericFieldParameters = numericFieldParameters;
 
         internal NumericField(FieldContainer? parent, FieldParameters common, int min, int max, int @default,
             int? divisor, int? multiplier, int? valueOffset, string? suffix, (int value, string text)? customValueFormatting)
-            : base(parent, common, min, max, @default) =>
-            (Divisor, Multiplier, ValueOffset, Suffix, CustomValueFormatting) =
-            (divisor, multiplier, valueOffset, suffix, customValueFormatting);
+            : this(parent, common, min, max, @default, new NumericFieldParameters(divisor, multiplier, valueOffset, suffix, customValueFormatting))
+        {
+        }
 
         internal override FieldBase WithParent(FieldContainer parent) =>
-            new NumericField(parent, Parameters, Min, Max, Default, Divisor, Multiplier, ValueOffset, Suffix, CustomValueFormatting);
+            new NumericField(parent, Parameters, Min, Max, Default, numericFieldParameters);
+
+        private class NumericFieldParameters
+        {
+            public (int value, string text)? CustomValueFormatting { get; }
+            public string? OffLabel { get; }
+            public int? Divisor { get; }
+            public int? Multiplier { get; }
+            public int? ValueOffset { get; }
+            public string? Suffix { get; }
+
+            internal NumericFieldParameters (int? divisor, int? multiplier, int? valueOffset, string? suffix, (int value, string text)? customValueFormatting) =>
+                (Divisor, Multiplier, ValueOffset, Suffix, CustomValueFormatting) =
+                (divisor, multiplier, valueOffset, suffix, customValueFormatting);
+        }
     }
 }
