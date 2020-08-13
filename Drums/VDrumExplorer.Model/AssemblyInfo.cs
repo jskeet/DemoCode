694a02ad6265ce096e820649a996ec9bcc7e5fe3
Jon Skeet <skeet@pobox.com>
2020-08-13 11:23:33 +0100

    Add skeleton of .NET Framework project for profiling

--- Drums/VDrumExplorer.Model/AssemblyInfo.cs
+++ Drums/VDrumExplorer.Model/AssemblyInfo.cs
@@ -5,3 +5,4 @@
 using System.Runtime.CompilerServices;
 [assembly:InternalsVisibleTo("VDrumExplorer.Model.Test")]
 [assembly:InternalsVisibleTo("VDrumExplorer.Console")]
+[assembly:InternalsVisibleTo("VDrumExplorer.NetFrameworkProfiling")]
---
+++ Drums/VDrumExplorer.NetFrameworkProfiling/Program.cs
@@ -0,0 +1,19 @@
+﻿// Copyright 2020 Jon Skeet. All rights reserved.
+// Use of this source code is governed by the Apache License 2.0,
+// as found in the LICENSE.txt file.
+
+namespace VDrumExplorer.NetFrameworkProfiling
+{
+    /// <summary>
+    /// For whatever reason, I've found it easiest to perform profiling
+    /// (e.g. with CodeTrack - https://www.getcodetrack.com/) via .NET Framework
+    /// console apps than with .NET Core. This project is expected to change
+    /// purpose several times, but should just do one thing that's easy to profile
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
