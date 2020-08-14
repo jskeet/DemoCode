37db1130c8df04f979679d874a1450d15b6262e5
Jon Skeet <skeet@pobox.com>
2020-08-14 18:03:26 +0100

    Allow custom values to be specified for numeric fields
    ... then use them for PreDelay. There are some other fields we might want to use this for too.

--- Drums/VDrumExplorer.Model/Schema/Fields/NumericField.cs
+++ Drums/VDrumExplorer.Model/Schema/Fields/NumericField.cs
@@ -2,6 +2,8 @@
 // Use of this source code is governed by the Apache License 2.0,
 // as found in the LICENSE.txt file.
 
+using System;
+using System.Collections.Generic;
 using VDrumExplorer.Model.Schema.Physical;
 using static System.FormattableString;
 
@@ -12,28 +14,37 @@ namespace VDrumExplorer.Model.Schema.Fields
     /// </summary>
     public sealed class NumericField : NumericFieldBase
     {
-        private readonly NumericFieldParameters numericFieldParameters;
+        private readonly INumericFieldFormatter numericFieldFormatter;
 
         private NumericField(FieldContainer? parent, FieldParameters common,
-            NumericFieldBaseParameters numericBaseParameters, NumericFieldParameters numericFieldParameters)
+            NumericFieldBaseParameters numericBaseParameters, INumericFieldFormatter numericFieldFormatter)
             : base(parent, common, numericBaseParameters) =>
-            this.numericFieldParameters = numericFieldParameters;
+            this.numericFieldFormatter = numericFieldFormatter;
 
         internal NumericField(FieldContainer? parent, FieldParameters common, int min, int max, int @default, NumericCodec codec,
             int? divisor, int? multiplier, int? valueOffset, string? suffix, (int value, string text)? customValueFormatting)
             : this(parent, common,
                   new NumericFieldBaseParameters(min, max, @default, codec),
-                  new NumericFieldParameters(divisor, multiplier, valueOffset, suffix, customValueFormatting))
+                  new ComputedValueFormatter(divisor, multiplier, valueOffset, suffix, customValueFormatting))
+        {
+        }
+
+        internal NumericField(FieldContainer? parent, FieldParameters common, int min, int max, int @default, NumericCodec codec, IReadOnlyList<string> values)
+            : this(parent, common, new NumericFieldBaseParameters(min, max, @default, codec), new CustomValueFormatter(values, min, max))
         {
         }
 
         internal override FieldBase WithParent(FieldContainer parent) =>
-            new NumericField(parent, Parameters, NumericBaseParameters, numericFieldParameters);
+            new NumericField(parent, Parameters, NumericBaseParameters, numericFieldFormatter);
 
+        internal string FormatRawValue(int rawValue) => numericFieldFormatter.FormatRawValue(rawValue);
 
-        internal string FormatRawValue(int rawValue) => numericFieldParameters.FormatRawValue(rawValue);
+        private interface INumericFieldFormatter
+        {
+            string FormatRawValue(int rawValue);
+        }
 
-        private class NumericFieldParameters
+        private class ComputedValueFormatter : INumericFieldFormatter
         {
             public (int value, string text)? CustomValueFormatting { get; }
             public string? OffLabel { get; }
@@ -42,11 +53,11 @@ namespace VDrumExplorer.Model.Schema.Fields
             public int? ValueOffset { get; }
             public string? Suffix { get; }
 
-            internal NumericFieldParameters (int? divisor, int? multiplier, int? valueOffset, string? suffix, (int value, string text)? customValueFormatting) =>
+            internal ComputedValueFormatter(int? divisor, int? multiplier, int? valueOffset, string? suffix, (int value, string text)? customValueFormatting) =>
                 (Divisor, Multiplier, ValueOffset, Suffix, CustomValueFormatting) =
                 (divisor, multiplier, valueOffset, suffix, customValueFormatting);
 
-            internal string FormatRawValue(int rawValue)
+            public string FormatRawValue(int rawValue)
             {
                 if (CustomValueFormatting is (int customValue, string customValueText) && rawValue == customValue)
                 {
@@ -63,6 +74,23 @@ namespace VDrumExplorer.Model.Schema.Fields
                 value *= Multiplier ?? 1;
                 return value / (Divisor ?? 1m);
             }
+        }
+
+        private class CustomValueFormatter : INumericFieldFormatter
+        {
+            private readonly int min;
+            private readonly IReadOnlyList<string> values;
+
+            internal CustomValueFormatter(IReadOnlyList<string> values, int min, int max)
+            {
+                (this.values, this.min) = (values, min);
+                if (values.Count != max - min + 1)
+                {
+                    throw new ArgumentException($"Expected {max - min + 1} values; got {values.Count}");
+                }
+            }
+
+            public string FormatRawValue(int rawValue) => values[rawValue - min];
         }
     }
 }
