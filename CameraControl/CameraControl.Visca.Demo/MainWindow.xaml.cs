8f0bd15c77794a7170af4860053a3cc3b63feb27
Jon Skeet <skeet@pobox.com>
2020-12-04 09:19:14 +0000

    First pass at publishing the VISCA controller code
    This is quite hacky at the moment in terms of encapsulation, but good enough for a blog post.

---
+++ CameraControl/CameraControl.Visca.Demo/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿// Copyright 2020 Jon Skeet. All rights reserved.
+// Use of this source code is governed by the Apache License 2.0,
+// as found in the LICENSE.txt file.
+
+using System;
+using System.Windows;
+using System.Windows.Input;
+
+namespace CameraControl.Visca.Demo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private ViscaController controller;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Connect(object sender, RoutedEventArgs e)
+        {
+            controller = new ViscaController(ipAddress.Text, 5678);
+            var rtspStream = WebEye.Stream.FromUri(
+                uri: new Uri(rtspUrl.Text),
+                connectionTimeout: TimeSpan.FromSeconds(10),
+                streamTimeout: TimeSpan.FromSeconds(10),
+                transport: WebEye.RtspTransport.Undefined,
+                flags: WebEye.RtspFlags.PreferTcp);
+            rtspStream.Start();
+            cameraStream.Stream = rtspStream;
+        }
+
+        // All of the controller calls are asynchronous, but we're effectively using them in a fire and forget manner
+        // anyway. There's no point in awaiting the task, as we don't do anything after it.
+
+        private void ContinuousUpLeft(object sender, MouseButtonEventArgs e) => ContinuousPanTilt(false, false);
+        private void ContinuousUp(object sender, MouseButtonEventArgs e) => ContinuousPanTilt(null, false);
+        private void ContinuousUpRight(object sender, MouseButtonEventArgs e) => ContinuousPanTilt(true, false);
+        private void ContinuousLeft(object sender, MouseButtonEventArgs e) => ContinuousPanTilt(false, null);
+        private void ContinuousRight(object sender, MouseButtonEventArgs e) => ContinuousPanTilt(true, null);
+        private void ContinuousDownLeft(object sender, MouseButtonEventArgs e) => ContinuousPanTilt(false, true);
+        private void ContinuousDown(object sender, MouseButtonEventArgs e) => ContinuousPanTilt(null, true);
+        private void ContinuousDownRight(object sender, MouseButtonEventArgs e) => ContinuousPanTilt(true, true);
+        private void StopContinuousPanTilt(object sender, MouseButtonEventArgs e) => controller?.ContinuousPanTilt(null, null, 1, 1, default);
+
+        private void ContinuousPanTilt(bool? pan, bool? tilt)
+        {
+            var speed = (byte) panTiltSpeed.Value;
+            controller?.ContinuousPanTilt(pan, tilt, speed, speed, default);
+        }
+
+        private void ContinuousZoomIn(object sender, MouseButtonEventArgs e) => controller?.ContinuousZoom(true, (byte) zoomSpeed.Value, default);
+        private void ContinuousZoomOut(object sender, MouseButtonEventArgs e) => controller?.ContinuousZoom(false, (byte) zoomSpeed.Value, default);
+        private void StopContinuousZoom(object sender, MouseButtonEventArgs e) => controller?.ContinuousZoom(null, 1, default);
+
+        private void MoveToHome(object sender, RoutedEventArgs e) => controller?.GoHome(default);
+    }
+}
---
+++ CameraControl/CameraControl.Visca/ReadBuffer.cs
@@ -0,0 +1,77 @@
+﻿// Copyright 2020 Jon Skeet. All rights reserved.
+// Use of this source code is governed by the Apache License 2.0,
+// as found in the LICENSE.txt file.
+
+using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CameraControl.Visca
+{
+    // TODO: This should probably contain the stream, and we should just create a new buffer when necessary.
+
+    /// <summary>
+    /// A read buffer for VISCA packets. 
+    /// </summary>
+    internal class ReadBuffer
+    {
+        private int size;
+        private readonly byte[] buffer = new byte[256];
+
+        internal void Clear()
+        {
+            size = 0;
+        }
+
+        internal async Task<byte[]> ReadAsync(Stream stream, CancellationToken cancellationToken)
+        {
+            while (true)
+            {
+                if (FindEnd() is int count)
+                {
+                    return Consume(count);
+                }
+                if (size == buffer.Length)
+                {
+                    throw new ViscaProtocolException($"Read {size} bytes without a reaching the end of a VISCA packet");
+                }
+                using (var registration = cancellationToken.Register(() => stream.Close()))
+                {
+                    int bytesRead = await stream.ReadAsync(buffer, size, buffer.Length - size);
+                    if (bytesRead == 0)
+                    {
+                        throw new ViscaProtocolException("Reached end of VISCA stream");
+                    }
+                    size += bytesRead;
+                }
+            }
+
+            int? FindEnd()
+            {
+                for (int i = 0; i < size; i++)
+                {
+                    if (buffer[i] == 0xff)
+                    {
+                        // Include the trailing byte
+                        return i + 1;
+                    }
+                }
+                return null;
+            }
+
+            byte[] Consume(int count)
+            {
+                byte[] ret = new byte[count];
+                Buffer.BlockCopy(buffer, 0, ret, 0, count);
+                size -= count;
+                // Shift the content of the array if we need to
+                if (size != 0)
+                {
+                    Buffer.BlockCopy(buffer, count, buffer, 0, size);
+                }
+                return ret;
+            }
+        }
+    }
+}
