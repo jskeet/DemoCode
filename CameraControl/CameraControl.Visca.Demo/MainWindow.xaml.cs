376323199c5985de51d868231db177cab68a1776
Jon Skeet <skeet@pobox.com>
2020-12-06 19:11:32 +0000

    Fancier camera controls with click+drag effect

--- CameraControl/CameraControl.Visca.Demo/MainWindow.xaml.cs
+++ CameraControl/CameraControl.Visca.Demo/MainWindow.xaml.cs
@@ -3,6 +3,7 @@
 // as found in the LICENSE.txt file.
 
 using System;
+using System.ComponentModel;
 using System.Windows;
 using System.Windows.Input;
 
@@ -11,13 +12,20 @@ namespace CameraControl.Visca.Demo
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, INotifyPropertyChanged
     {
         private ViscaController controller;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public int PanSpeed { get; private set; } = 0;
+        public int TiltSpeed { get; private set; } = 0;
+        public int ZoomSpeed { get; private set; } = 0;
 
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = this;
         }
 
         private void Connect(object sender, RoutedEventArgs e)
@@ -33,29 +41,90 @@ namespace CameraControl.Visca.Demo
             cameraStream.Stream = rtspStream;
         }
 
-        // All of the controller calls are asynchronous, but we're effectively using them in a fire and forget manner
-        // anyway. There's no point in awaiting the task, as we don't do anything after it.
-
-        private void ContinuousUpLeft(object sender, MouseButtonEventArgs e) => ContinuousPanTilt(false, false);
-        private void ContinuousUp(object sender, MouseButtonEventArgs e) => ContinuousPanTilt(null, false);
-        private void ContinuousUpRight(object sender, MouseButtonEventArgs e) => ContinuousPanTilt(true, false);
-        private void ContinuousLeft(object sender, MouseButtonEventArgs e) => ContinuousPanTilt(false, null);
-        private void ContinuousRight(object sender, MouseButtonEventArgs e) => ContinuousPanTilt(true, null);
-        private void ContinuousDownLeft(object sender, MouseButtonEventArgs e) => ContinuousPanTilt(false, true);
-        private void ContinuousDown(object sender, MouseButtonEventArgs e) => ContinuousPanTilt(null, true);
-        private void ContinuousDownRight(object sender, MouseButtonEventArgs e) => ContinuousPanTilt(true, true);
-        private void StopContinuousPanTilt(object sender, MouseButtonEventArgs e) => controller?.ContinuousPanTilt(null, null, 1, 1, default);
-
-        private void ContinuousPanTilt(bool? pan, bool? tilt)
-        {
-            var speed = (byte) panTiltSpeed.Value;
-            controller?.ContinuousPanTilt(pan, tilt, speed, speed, default);
-        }
-
-        private void ContinuousZoomIn(object sender, MouseButtonEventArgs e) => controller?.ContinuousZoom(true, (byte) zoomSpeed.Value, default);
-        private void ContinuousZoomOut(object sender, MouseButtonEventArgs e) => controller?.ContinuousZoom(false, (byte) zoomSpeed.Value, default);
-        private void StopContinuousZoom(object sender, MouseButtonEventArgs e) => controller?.ContinuousZoom(null, 1, default);
+        private void StopContinuousPanTilt(object sender, MouseButtonEventArgs e) => SetPanTiltSpeed(0, 0);
+        private void StopContinuousZoom(object sender, MouseButtonEventArgs e) => SetZoomSpeed(0);
 
         private void MoveToHome(object sender, RoutedEventArgs e) => controller?.GoHome(default);
+
+        private void AdjustContinuousPanTilt(object sender, MouseButtonEventArgs e) =>
+            AdjustContinuousPanTilt(sender, (MouseEventArgs) e);
+
+        private void AdjustContinuousPanTilt(object sender, MouseEventArgs e)
+        {
+            if (e.LeftButton != MouseButtonState.Pressed)
+            {
+                return;
+            }
+            var position = e.GetPosition(panButton);
+
+            int panSpeed = NormalizeSpeed(position.X, panButton.ActualWidth, ViscaController.MaxPanTiltSpeed);
+            int tiltSpeed = NormalizeSpeed(position.Y, panButton.ActualHeight, ViscaController.MaxPanTiltSpeed);
+            SetPanTiltSpeed(panSpeed, tiltSpeed);
+        }
+
+        private void AdjustContinuousZoom(object sender, MouseButtonEventArgs e) =>
+            AdjustContinuousZoom(sender, (MouseEventArgs) e);
+
+        private void AdjustContinuousZoom(object sender, MouseEventArgs e)
+        {
+            if (e.LeftButton != MouseButtonState.Pressed)
+            {
+                return;
+            }
+            var position = e.GetPosition(zoomButton);
+            int zoomSpeed = NormalizeSpeed(position.X, zoomButton.ActualWidth, ViscaController.MaxZoomSpeed);
+            SetZoomSpeed(zoomSpeed);
+        }
+
+        private void SetPanTiltSpeed(int panSpeed, int tiltSpeed)
+        {
+            if (panSpeed == PanSpeed && tiltSpeed == TiltSpeed)
+            {
+                return;
+            }
+            bool? panDirection = panSpeed == 0 ? default(bool?) : panSpeed > 0;
+            bool? tiltDirection = tiltSpeed == 0 ? default(bool?) : tiltSpeed > 0;
+            controller?.ContinuousPanTilt(panDirection, tiltDirection, AbsSpeed(panSpeed), AbsSpeed(tiltSpeed), default);
+            PanSpeed = panSpeed;
+            TiltSpeed = tiltSpeed;
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(PanSpeed)));
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(TiltSpeed)));
+        }
+
+        private void SetZoomSpeed(int zoomSpeed)
+        {
+            if (zoomSpeed == ZoomSpeed)
+            {
+                return;
+            }
+            bool? zoomDirection = zoomSpeed == 0 ? default(bool?) : zoomSpeed > 0;
+            controller?.ContinuousZoom(zoomDirection, AbsSpeed(zoomSpeed), default);
+            ZoomSpeed = zoomSpeed;
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(ZoomSpeed)));
+        }
+
+        /// <summary>
+        /// Normalizes a speed to the range [-maxValue, maxValue], assuming that <paramref name="position"/>
+        /// should be in the range [0, visualScale] (but might be outside that range, in which case
+        /// the maximum value should be used).
+        /// </summary>
+        private static int NormalizeSpeed(double position, double visualScale, int maxValue)
+        {
+            // Scale to -1 / +1
+            double scaledPosition = (position / visualScale) * 2 - 1;
+            int speed = (int) (scaledPosition * (maxValue + 1));
+            speed = Math.Min(speed, maxValue);
+            speed = Math.Max(speed, -maxValue);
+            return speed;
+        }
+
+        /// <summary>
+        /// Works out the speed to use in the VISCA packet, assuming that a speed
+        /// of 0 should become 1 (as other code will have set the direction to ignore the
+        /// value) and negative values should become positive.
+        /// </summary>
+        private static byte AbsSpeed(int speed) => (byte) Math.Max(Math.Abs(speed), 1);
+
+        private void Reset(object sender, RoutedEventArgs e) => controller?.GoHome();
     }
 }
