224c33b04c91a9af9382cf072f00f49d3db2857e
Jon Skeet <skeet@pobox.com>
2021-06-27 07:49:43 +0100

    First drop of IconPlatform code
    (Will write a blog post when I've got the D2 text working.)

---
+++ IconPlatform/IconPlatform.ConsoleDemo/Program.cs
@@ -0,0 +1,30 @@
+﻿using IconPlatform.Model;
+using System;
+using System.Threading.Tasks;
+
+namespace IconPlatform.ConsoleDemo
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var controller = await PlatformMXController.ConnectAsync("Platform M+ V2.15");
+
+            controller.ButtonChanged += (sender, args) =>
+            {
+                Console.WriteLine($"Channel {args.Channel} button {args.Button} {(args.Down ? "pressed" : "released")}");
+                controller.SetLight(args.Channel, args.Button, args.Down);
+            };
+            controller.KnobTurned += (sender, args) =>
+            {
+                Console.WriteLine($"Channel {args.Channel} knob turned with value {args.Value}");
+            };
+            controller.FaderMoved += (sender, args) =>
+            {
+                Console.WriteLine($"Channel {args.Channel} fader moved to {args.Position}");
+                controller.MoveFader(args.Channel, args.Position);
+            };
+            await Task.Delay(60000);
+        }
+    }
+}
---
+++ IconPlatform/IconPlatform.Model/ButtonEventArgs.cs
@@ -0,0 +1,31 @@
+﻿// Copyright 2021 Jon Skeet. All rights reserved.
+// Use of this source code is governed by the Apache License 2.0,
+// as found in the LICENSE.txt file.
+
+namespace IconPlatform.Model
+{
+    /// <summary>
+    /// Information provided when a button is pressed or released.
+    /// </summary>
+    public sealed class ButtonEventArgs
+    {
+        /// <summary>
+        /// The 1-based channel containing the button.
+        /// </summary>
+        public int Channel { get; }
+
+        /// <summary>
+        /// The button type.
+        /// </summary>
+        public ButtonType Button { get; }
+
+        /// <summary>
+        /// True if this event is for a button being pressed; false if
+        /// it is for a button being released.
+        /// </summary>
+        public bool Down { get; }
+
+        public ButtonEventArgs(int channel, ButtonType button, bool down) =>
+            (Channel, Button, Down) = (channel, button, down);
+    }
+}
