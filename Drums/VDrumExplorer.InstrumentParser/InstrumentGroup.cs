20f4153914a04af61e375e88d4bfd838449f4742
Jon Skeet <skeet@pobox.com>
2021-10-30 11:50:51 +0100

    Add an instrument text file parser to make it easier to add new modules

---
+++ Drums/VDrumExplorer.InstrumentParser/InstrumentGroup.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace VDrumExplorer.InstrumentParser
+{
+    public class InstrumentGroup
+    {
+        [JsonProperty("description")]
+        public string Description { get; set; }
+        [JsonProperty("veditCategory")]
+        public string VEditCategory { get; set; }
+        [JsonProperty("instruments")]
+        public SortedDictionary<int, string> Instruments { get; set; }
+    }
+}
