5477ebbe1e0529a1da8e174038a7ab421589d013
Jon Skeet <skeet@pobox.com>
2021-11-03 18:39:21 +0000

    First pass at DMX code. More to come. (But this does work with actual hardware...)

---
+++ DmxLighting/DmxLighting.ConsoleApp/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Threading;
+
+namespace DmxLighting.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1-37 = Fusion Orbit
+            // 38-43 = Latta Alvor
+            var universe = new DmxUniverse(0, 43);
+
+            
+            var sender = new StreamingAcnSender("192.168.1.46");
+
+            // Latta Alvor:
+            // Manual mode, full total brightness, with half red/blue and full green
+            universe.SetChannelValues(38, new byte[] { 255, 127, 255, 127, 0, 0 });
+
+            // Fusion orbit
+            // Channels 1-5 are for pan/tilt; leave at 0.
+            universe[6] = 255;   // Master dimmer: full brightness
+
+            // Beam
+            universe[7] = 0;     // No strobe
+            universe[8] = 0;     // No colour macro
+            universe[9] = 127;   // Red
+            universe[10] = 255;  // Green
+            universe[11] = 127;  // Blue
+            universe[12] = 31;   // White
+
+            // LED Ring
+            universe[13] = 255;  // Master dimmer: full brightness
+            universe[14] = 0;    // No strobe
+            universe[15] = 0;    // No colour macro
+            universe[16] = 0;    // No colour program
+            universe[17] = 0;    // No macro progam speed
+
+            universe[18] = 255;  // Segment 1: all red
+            universe[22] = 255;  // Segment 2: all green
+            universe[26] = 255;  // Segment 3: all blue
+
+            universe[26] = 127;  // Segment 4: half-brightness R+G+B
+            universe[27] = 127;
+            universe[28] = 127;
+
+            universe[29] = 31;   // Segment 5: dull red
+            universe[34] = 31;   // Segment 6: dull green
+
+            universe[36] = 0;    // No "show"
+            universe[37] = 0;    // No sound sensitivity
+
+            sender.SendUniverse(universe);
+
+            // Pan to max
+            Console.ReadLine();
+            universe[1] = 255;
+            sender.SendUniverse(universe);
+
+            // Tilt to max
+            Console.ReadLine();
+            universe[3] = 255;
+            sender.SendUniverse(universe);
+
+            // Reset back, with all red
+            Console.ReadLine();
+            universe[1] = 0;
+            universe[3] = 0;
+            universe[9] = 255;
+            universe[10] = 0;
+            universe[11] = 0;
+            universe[12] = 0;
+
+            for (int ring = 0; ring < 6; ring++)
+            {
+                universe[ring * 3 + 18] = 255;
+                universe[ring * 3 + 19] = 0;
+                universe[ring * 3 + 20] = 0;
+            }
+            sender.SendUniverse(universe);
+
+            // Move it around a bit
+            Console.ReadLine();
+            universe.SetChannelValues(38, new byte[] { 255, 0, 0, 255, 0, 0 });
+            universe[1] = 10;
+            universe[3] = 10;
+            sender.SendUniverse(universe);
+            Thread.Sleep(2000);
+
+            universe.SetChannelValues(38, new byte[] { 255, 0, 255, 0, 0, 0 });
+            universe[1] = 30;
+            universe[3] = 30;
+            sender.SendUniverse(universe);
+            Thread.Sleep(2000);
+
+            universe.SetChannelValues(38, new byte[] { 255, 255, 0, 0, 0, 0 });
+            universe[1] = 40;
+            universe[3] = 0;
+            sender.SendUniverse(universe);
+
+            // Turn everything off
+            Console.ReadLine();
+            for (int i = 1; i <= 43; i++)
+            {
+                universe[i] = 0;
+            }
+            sender.SendUniverse(universe);
+        }
+    }
+}
---
+++ DmxLighting/DmxLighting/DmxUniverse.cs
@@ -0,0 +1,110 @@
+﻿// Copyright 2021 Jon Skeet. All rights reserved.
+// Use of this source code is governed by the Apache License 2.0,
+// as found in the LICENSE.txt file.
+using System;
+using System.Linq;
+
+namespace DmxLighting
+{
+    // TODO: Consider using spans, if AYS will be okay with that.
+    // (Need to check target versions.)
+
+    /// <summary>
+    /// A DMX universe of up to 512 channels, each with a byte value
+    /// (0-255).
+    /// </summary>
+    public sealed class DmxUniverse
+    {
+        /// <summary>
+        /// The maximum size of a DMX universe. Smaller universes
+        /// can be created when fewer channels are needed, leading
+        /// to smaller network packets being transmitted.
+        /// </summary>
+        public const int MaxSize = 512;
+
+        /// <summary>
+        /// The values for the channels. The first byte will always
+        /// be zero, to allow simple 1-based indexing.
+        /// </summary>
+        private readonly byte[] channels;
+
+        /// <summary>
+        /// Raised when the data in the channels has changed.
+        /// Event handlers should not modify the byte array as it is
+        /// shared between all handlers, but it does not represent the
+        /// "raw" underlying array.
+        /// </summary>
+        public EventHandler<byte[]> ChannelsChanged;
+
+        /// <summary>
+        /// The number of the universe.
+        /// </summary>
+        public short UniverseNumber { get; }
+
+        /// <summary>
+        /// Retrieves or sets the value of a single channel.
+        /// </summary>
+        /// <param name="channel">The 1-based channel to retrieve.</param>
+        /// <returns>The value of the channel.</returns>
+        public byte this[int channel]
+        {
+            get => channels[channel];
+            // TODO: Make this more efficient!
+            set => SetChannelValues(channel, new[] { value });
+        }
+
+        /// <summary>
+        /// Creates a universe with a universe number of 1, and the maximum
+        /// size.
+        /// </summary>
+        public DmxUniverse() : this(1, MaxSize)
+        {
+        }
+
+        /// <summary>
+        /// Creates a universe with the given size.
+        /// </summary>
+        /// <param name="size">The size of the universe, as the number of channels it supports.</param>
+        public DmxUniverse(short universeNumber, int size)
+        {
+            UniverseNumber = universeNumber; // TODO: validate
+            if (size < 1 || size > MaxSize)
+            {
+                throw new ArgumentOutOfRangeException(nameof(size));
+            }
+            // One larger to make 1-based indexing simpler.
+            channels = new byte[size + 1];
+        }
+
+        /// <summary>
+        /// Returns a clone of the current channel data.
+        /// </summary>
+        /// <returns>The values of the channels. The 0th element of the array is unused;
+        /// channel 1 is stored in element 1 etc.</returns>
+        public byte[] CloneChannels() => channels.ToArray();
+
+        /// <summary>
+        /// Sets one or more channel values.
+        /// </summary>
+        /// <param name="startChannel">The first channel to modify; 1-based.</param>
+        /// <param name="values">The new values from <paramref name="startChannel"/> onwards.</param>
+        public void SetChannelValues(int startChannel, byte[] values)
+        {
+            Buffer.BlockCopy(values, 0, channels, startChannel, values.Length);
+            ChannelsChanged?.Invoke(this, channels.ToArray());
+        }
+
+        /// <summary>
+        /// Modifiers channel values atomically (with respect to <see cref="ChannelsChanged"/>).
+        /// </summary>
+        /// <param name="modifier"></param>
+        public void SetChannelValues(Action<byte[]> modifier)
+        {
+            var clone = channels.ToArray();
+            modifier(clone);
+            Buffer.BlockCopy(clone, 0, channels, 0, channels.Length);
+            // We don't need to clone again, unless something's really 
+            ChannelsChanged?.Invoke(this, clone);
+        }
+    }
+}
