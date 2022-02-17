1aa78a28f6df341e934331f4d5e3eeed84bf3765
Jon Skeet <skeet@pobox.com>
2022-02-17 19:56:28 +0000

    Add more copyright comments

--- CameraControl/CameraControl.Visca/Preconditions.cs
+++ CameraControl/CameraControl.Visca/Preconditions.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿// Copyright 2020 Jon Skeet. All rights reserved.
+// Use of this source code is governed by the Apache License 2.0,
+// as found in the LICENSE.txt file.
+using System;
 using System.RuntimeCompilerServices;
 
 namespace CameraControl.Visca
