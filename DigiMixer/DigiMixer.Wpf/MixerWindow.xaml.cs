67d808609354bcb3594933cc34f35696f628e61f
Jon Skeet <skeet@pobox.com>
2022-10-25 19:23:38 +0100

    Dispose of the mixer when the mixer window is closed

--- DigiMixer/DigiMixer.Wpf/MixerWindow.xaml.cs
+++ DigiMixer/DigiMixer.Wpf/MixerWindow.xaml.cs
@@ -1,16 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace DigiMixer.Wpf;
 /// <summary>
@@ -21,5 +10,14 @@ public partial class MixerWindow : Window
     public MixerWindow()
     {
         InitializeComponent();
+    }
+
+    protected override void OnClosed(EventArgs e)
+    {
+        base.OnClosed(e);
+        if (DataContext is IDisposable disp)
+        {
+            disp.Dispose();
+        }
     }
 }
