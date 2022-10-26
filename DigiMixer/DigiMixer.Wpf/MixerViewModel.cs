9f72cd4ecba61cafc00ec52d6ad38dc522ef0af1
Jon Skeet <skeet@pobox.com>
2022-10-26 05:35:58 +0100

    Add implicit usings to the WPF project

--- DigiMixer/DigiMixer.Wpf/MixerViewModel.cs
+++ DigiMixer/DigiMixer.Wpf/MixerViewModel.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
+﻿using System.ComponentModel;
 using System.Windows.Threading;
 
 namespace DigiMixer.Wpf;
