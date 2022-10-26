fa5295b046e7f0c32e3258871c7a1092e8a017b9
Jon Skeet <skeet@pobox.com>
2022-10-26 11:15:21 +0100

    Add log files to DigiMixer

--- DigiMixer/DigiMixer.Wpf/App.xaml.cs
+++ DigiMixer/DigiMixer.Wpf/App.xaml.cs
@@ -1,11 +1,50 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using DigiMixer.Wpf.Utilities;
+using Microsoft.Extensions.Logging;
 using System.IO;
 using System.Windows;
 
 namespace DigiMixer.Wpf;
-/// <summary>
-/// Interaction logic for App.xaml
-/// </summary>
+
 public partial class App : Application
 {
+    internal MemoryLoggerProvider Log { get; private set; }
+
+    public static new App Current => (App) Application.Current;
+
+    public App()
+    {
+        Log = new MemoryLoggerProvider();
+    }
+
+    protected override void OnExit(ExitEventArgs e)
+    {
+        base.OnExit(e);
+        var logsDirectory = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "DigiMixer", "Logs");
+        Log.SaveToDirectory(logsDirectory, "log");
+    }
+
+    internal class MemoryLogger : ILogger
+    {
+        private List<string> logEntries = new List<string>();
+
+        public IDisposable BeginScope<TState>(TState state) => Task.CompletedTask;
+
+        public bool IsEnabled(LogLevel logLevel) => true;
+
+        public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter)
+        {
+            lock (logEntries)
+            {
+                logEntries.Add(formatter(state, exception));
+            }
+        }
+
+        public void Save(string path)
+        {
+            lock (logEntries)
+            {
+                File.WriteAllLines(path, logEntries);
+            }
+        }
+    }
 }
