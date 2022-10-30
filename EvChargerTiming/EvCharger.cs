3863a07fd08a986861e325d86e4042a9e04daf8e
Jon Skeet <skeet@pobox.com>
2022-10-30 16:12:39 +0000

    Demo code for EV charging blog post

---
+++ EvChargerTiming/EvCharger.cs
@@ -0,0 +1,28 @@
+﻿// Copyright 2022 Jon Skeet. All rights reserved.
+// Use of this source code is governed by the Apache License 2.0,
+// as found in the LICENSE.txt file.
+
+namespace EvChargerTiming;
+
+public class EvCharger
+{
+    /// <summary>
+    /// Whether or not the charger is currently "on" in terms of
+    /// being able to supply power. Somewhat orthogonally, the real
+    /// system would be able to report whether a car was actually plugged
+    /// in or not, how much current was being drawn etc.
+    /// </summary>
+    public bool On { get; private set; }
+
+    /// <summary>
+    /// Method to change to the given state.
+    /// (This could be a property setter, or two different methods...
+    /// all kinds of different design options here which aren't relevant
+    /// to the point of this sample code.)
+    /// </summary>
+    public void ChangeState(bool on)
+    {
+        // Probably some logging here...
+        On = on;
+    }
+}
---
+++ EvChargerTiming/EvChargerController.cs
@@ -0,0 +1,68 @@
+﻿// Copyright 2022 Jon Skeet. All rights reserved.
+// Use of this source code is governed by the Apache License 2.0,
+// as found in the LICENSE.txt file.using NodaTime;
+
+using Microsoft.Extensions.Logging;
+using NodaTime;
+using NodaTime.Text;
+
+namespace EvChargerTiming;
+
+public class EvChargerController
+{
+    // Note: Noda Time exposes a ZonedClock which could be used for this,
+    // which just composes a clock and a zone, but I've separated them out for
+    // clarity.
+    private readonly DateTimeZone zone;
+    private readonly IClock clock;
+    private readonly ChargingSchedule schedule;
+    private readonly EvCharger charger;
+    private readonly ILogger logger;
+
+    public EvChargerController(EvCharger charger, ChargingSchedule schedule, DateTimeZone zone, IClock clock, ILogger logger)
+    {
+        this.charger = charger;
+        this.schedule = schedule;
+        this.zone = zone;
+        this.clock = clock;
+        this.logger = logger;
+    }
+
+    /// <summary>
+    /// Infinite loop which just checks periodically whether the charger should be on or not,
+    /// based on the schedule.
+    /// </summary>
+    /// <param name="pollingInterval">The interval at which to check whether or not the charger should be on.</param>
+    public void MainLoop(TimeSpan pollingInterval)
+    {
+        // In a real system we'd want ways of shutting down, updating the schedule,
+        // changing the system time zone, updating the time zone database etc.
+        while (true)
+        {
+            Instant now = clock.GetCurrentInstant();
+
+            // Note: converting an instant into a local date/time is always unambiguous. (Every
+            // instant maps to exactly one local date/time.) Conversions in the opposite
+            // direction may be ambiguous or invalid.
+            ZonedDateTime nowInTimeZone = now.InZone(zone);
+
+            bool shouldBeOn = schedule.IsChargingEnabled(nowInTimeZone.LocalDateTime);
+            if (charger.On != shouldBeOn)
+            {
+                logger.LogInformation("At {now} ({local} local), changing state to {state}",
+                    InstantPattern.ExtendedIso.Format(now),
+                    ZonedDateTimePattern.GeneralFormatOnlyIso.Format(nowInTimeZone),
+                    shouldBeOn);
+
+                charger.ChangeState(shouldBeOn);
+            }
+
+            // We *could* predict when we'll next need to turn the charger on.
+            // However, that's significantly more fiddly than just checking periodically.
+            // A check of something "once per minute" will take very little power,
+            // and be much simpler than trying to predict how long to sleep for.
+            // The reality of EV charing 
+            Thread.Sleep(pollingInterval);
+        }
+    }
+}
