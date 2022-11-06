1727b8994262e00597ba5d3db5257682693220e2
Jon Skeet <skeet@pobox.com>
2022-11-06 11:34:20 +0000

    Add a proxy for the RCF M-18
    This allows multiple clients to connect even from the same host.
    It does mean all clients receive all messages, but that's generally okay.

---
+++ DigiMixer/DigiMixer.RcfProxy/OscMessageExtensions.cs
@@ -0,0 +1,27 @@
+﻿using OscCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DigiMixer.RcfProxy;
+
+internal static class OscMessageExtensions
+{
+    internal static string ToLogFormat(this OscMessage message) =>
+        message.Count == 0 ? message.Address : $"{message.Address} {string.Join(", ", message.Select(FormatParameter))}";
+
+    private static string FormatParameter(object param) => param switch
+    {
+        string x => $"\"{x}\"",
+        float f => $"{f}F",
+        double d => $"{d}D",
+        int i => $"{i}I",
+        long l => $"{l}L",
+        byte b => $"0x{b:X2}V",
+        byte[] array => $"(bytes) {Convert.ToBase64String(array)}",
+        null => "null",
+        _ => param.ToString() ?? "null"
+    };
+}
