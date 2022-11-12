87cc1663a2bac9ed1914daa98f55c8a4b13f9686
Jon Skeet <skeet@pobox.com>
2022-11-12 10:37:57 +0000

    First pass at Mackie code

---
+++ DigiMixer/DigiMixer.MackieDump/Buffer.cs
@@ -0,0 +1,42 @@
+﻿using DigiMixer.Mackie;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DigiMixer.MackieDump;
+
+internal class PacketBuffer
+{
+    private readonly string name;
+    private byte[] buffer = new byte[65536];
+    private int position = 0;
+
+    internal PacketBuffer(string name)
+    {
+        this.name = name;
+    }
+
+    internal void Consume(byte[] data)
+    {
+        Buffer.BlockCopy(data, 0, buffer, position, data.Length);
+        position += data.Length;
+
+        while (position != 0 && MackiePacket.TryParse(buffer, 0, position) is MackiePacket packet)
+        {
+            Console.WriteLine($"{name}: {packet}");
+            if (position == packet.Length)
+            {
+                position = 0;
+            }
+            else
+            {
+                Buffer.BlockCopy(buffer, packet.Length, buffer, 0, position - packet.Length);
+                position -= packet.Length;
+            }
+        }
+    }
+}
---
+++ DigiMixer/DigiMixer.MackieDump/Program.cs
@@ -0,0 +1,49 @@
+﻿using DigiMixer.MackieDump;
+
+if (args.Length != 1)
+{
+    Console.WriteLine("Please specify a text file to parse");
+    return;
+}
+
+var lines = File.ReadAllLines(args[0]);
+
+PacketBuffer clientBuffer = new PacketBuffer("Client");
+PacketBuffer mixerBuffer = new PacketBuffer("Mixer ");
+
+foreach (var line in lines)
+{
+    var parsed = ParseLine(line);
+    if (parsed is null)
+    {
+        continue;
+    }
+    var buffer = parsed.IsMixer ? mixerBuffer : clientBuffer;
+    buffer.Consume(parsed.Data);
+}
+
+Line? ParseLine(string line)
+{
+    if (line.Trim() == "")
+    {
+        return null;
+    }
+    bool isMixer = line.StartsWith("    ");
+    if (isMixer)
+    {
+        // Remove the indentation
+        line = line[4..];
+    }
+
+    // Remove the index within the stream
+    line = line[10..];
+    int indexOfText = line.IndexOf("   ");
+    // Remove the text at the end
+    line = line[..indexOfText];
+    byte[] data = line.Split(' ', StringSplitOptions.RemoveEmptyEntries)
+        .Select(part => Convert.ToByte(part, 16))
+        .ToArray();
+    return new Line(isMixer, data);
+}
+
+record Line(bool IsMixer, byte[] Data);
