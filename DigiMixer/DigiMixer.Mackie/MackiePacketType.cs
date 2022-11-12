bdcd86af75676cf46d4fa80456e6cb51d9538d87
Jon Skeet <skeet@pobox.com>
2022-11-12 10:37:57 +0000

    First pass at Mackie code

---
+++ DigiMixer/DigiMixer.Mackie/MackiePacketType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DigiMixer.Mackie;
+
+public enum MackiePacketType : byte
+{
+    Request = 0,
+    Response = 1,
+    Broadcast = 8
+}
