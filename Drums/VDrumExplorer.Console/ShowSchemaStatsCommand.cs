fcddbc8f2fa878eef8995f7b3afbd0e45843c504
Jon Skeet <skeet@pobox.com>
2023-03-09 21:06:07 +0000

    Add the ability to show the stats of all schemas

--- Drums/VDrumExplorer.Console/ShowSchemaStatsCommand.cs
+++ Drums/VDrumExplorer.Console/ShowSchemaStatsCommand.cs
@@ -2,6 +2,9 @@
 // Use of this source code is governed by the Apache License 2.0,
 // as found in the LICENSE.txt file.
 
+using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.CommandLine;
 using System.CommandLine.Invocation;
 using System.CommandLine.IO;
@@ -24,40 +27,57 @@ namespace VDrumExplorer.Console
             Description = "Shows statistics about a module schema",
             Handler = new ShowSchemaStatsCommand(),
         }
-        .AddRequiredOption<string>("--schema", "Name of schema to show, e.g. TD-27");
+        .AddRequiredOption<string>("--schema", "Name of schema to show, e.g. TD-27, or 'all'");
 
         public Task<int> InvokeAsync(InvocationContext context)
         {
+            var schemas = GetSchemasToDisplay(context);
+            if (schemas.Length == 0)
+            {
+                return Task.FromResult(1);
+            }
+            var console = context.Console.Out;
+            foreach (var schema in schemas)
+            {
+                var allContainers = schema.PhysicalRoot.DescendantsAndSelf();
+                var fieldContainers = allContainers.OfType<FieldContainer>();
+                var containerContainers = allContainers.OfType<ContainerContainer>();
+                var allFields = fieldContainers.SelectMany(fc => fc.Fields);
+                var distinctFields = allFields.Distinct();
+                var allOverlayFields = allFields.OfType<OverlayField>().SelectMany(overlay => overlay.FieldLists.Values.SelectMany(fl => fl.Fields));
+                var allDistinctOverlayFields = allOverlayFields.Distinct();
+
+                console.WriteLine($"Statistics for {schema.Identifier}:");
+                console.WriteLine($"Containers: {allContainers.Count()}");
+                console.WriteLine($"Field containers: {fieldContainers.Count()}");
+                console.WriteLine($"Container containers: {containerContainers.Count()}");
+                console.WriteLine($"All (non-overlaid) fields: {allFields.Count()}");
+                console.WriteLine($"Distinct (non-overlaid) fields: {distinctFields.Count()}");
+                console.WriteLine($"All overlaid fields: {allOverlayFields.Count()}");
+                console.WriteLine($"Distinct overlaid fields: {allDistinctOverlayFields.Count()}");
+                console.WriteLine($"Logical node descendants: {schema.LogicalRoot.DescendantsAndSelf().Count()}");
+                console.WriteLine();
+            }
+
+            return Task.FromResult(0);
+        }
+
+        private ModuleSchema[] GetSchemasToDisplay(InvocationContext context)
+        {
             var console = context.Console.Out;
             var schemaName = context.ParseResult.ValueForOption<string>("schema");
+            if (schemaName == "all")
+            {
+                return ModuleSchema.KnownSchemas.Values.Select(v => v.Value).ToArray();
+            }
             var identifier = ModuleSchema.KnownSchemas.Keys.FirstOrDefault(id => id.Name == schemaName);
             if (identifier is null)
             {
                 console.WriteLine($"Unknown schema name: '{schemaName}'");
-                return Task.FromResult(1);
+                return new ModuleSchema[0];
             }
             var schema = ModuleSchema.KnownSchemas[identifier].Value;
-
-            var allContainers = schema.PhysicalRoot.DescendantsAndSelf();
-            var fieldContainers = allContainers.OfType<FieldContainer>();
-            var containerContainers = allContainers.OfType<ContainerContainer>();
-            var allFields = fieldContainers.SelectMany(fc => fc.Fields);
-            var distinctFields = allFields.Distinct();
-            var allOverlayFields = allFields.OfType<OverlayField>().SelectMany(overlay => overlay.FieldLists.Values.SelectMany(fl => fl.Fields));
-            var allDistinctOverlayFields = allOverlayFields.Distinct();
-
-            console.WriteLine($"Statistics for {identifier.Name}:");
-            console.WriteLine($"Containers: {allContainers.Count()}");
-            console.WriteLine($"Field containers: {fieldContainers.Count()}");
-            console.WriteLine($"Container containers: {containerContainers.Count()}");
-            console.WriteLine($"All (non-overlaid) fields: {allFields.Count()}");
-            console.WriteLine($"Distinct (non-overlaid) fields: {distinctFields.Count()}");
-            console.WriteLine($"All overlaid fields: {allOverlayFields.Count()}");
-            console.WriteLine($"Distinct overlaid fields: {allDistinctOverlayFields.Count()}");
-
-            console.WriteLine($"Logical node descendants: {schema.LogicalRoot.DescendantsAndSelf().Count()}");
-
-            return Task.FromResult(0);
+            return new[] { schema };
         }
     }
 }
