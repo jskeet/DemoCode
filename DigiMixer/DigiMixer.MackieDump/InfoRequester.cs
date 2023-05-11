0057c1da06a8b1c63918cfc54b4a642dbc59ec12
Jon Skeet <skeet@pobox.com>
2023-05-11 18:33:24 +0100

    Add a new "info" command for MackieDump
    This allows us to dynamically request different info packets.

---
+++ DigiMixer/DigiMixer.MackieDump/InfoRequester.cs
@@ -0,0 +1,58 @@
+﻿using DigiMixer.Mackie.Core;
+using Google.Protobuf;
+using Microsoft.Extensions.Logging.Abstractions;
+
+namespace DigiMixer.MackieDump;
+
+internal class InfoRequester
+{
+    internal static async Task ExecuteAsync(string address, int port, string file, List<byte> infoItems)
+    {
+        PacketCollection pc = new PacketCollection();
+        using var controller = new MackieController(NullLogger.Instance, address, port);
+        controller.PacketSent += (sender, packet) => RecordPacket(packet, true);
+        controller.PacketReceived += (sender, packet) => RecordPacket(packet, false);
+
+        controller.MapCommand(MackieCommand.ClientHandshake, _ => new byte[] { 0x10, 0x40, 0xf0, 0x1d, 0xbc, 0xa2, 0x88, 0x1c });
+        controller.MapCommand(MackieCommand.GeneralInfo, _ => new byte[] { 0, 0, 0, 2, 0, 0, 0x40, 0 });
+        controller.MapCommand(MackieCommand.ChannelInfoControl, packet => new MackiePacketBody(packet.Body.Data.Slice(0, 4)));
+        await controller.Connect(default);
+        controller.Start();
+
+        // From MackieMixerApi.Connect
+        CancellationToken cancellationToken = default;
+        await controller.SendRequest(MackieCommand.KeepAlive, MackiePacketBody.Empty, cancellationToken);
+        await controller.SendRequest(MackieCommand.ClientHandshake, MackiePacketBody.Empty, cancellationToken);
+
+        foreach (var item in infoItems)
+        {
+            await controller.SendRequest(MackieCommand.GeneralInfo, new byte[] { 0, 0, 0, item }, cancellationToken);
+        }
+
+        Console.WriteLine($"Captured {pc.Packets.Count} packets");
+
+        using var output = File.Create(file);
+        pc.WriteTo(output);
+
+        void RecordPacket(MackiePacket packet, bool outbound)
+        {
+            pc.Packets.Add(Packet.FromMackiePacket(packet, outbound, null));
+            // Immediate uninterpreted display, truncated after 16 bytes of data.
+            var padding = outbound ? "" : "    ";
+            if (packet.Body.Data.Length == 0)
+            {
+                Console.WriteLine($"{DateTime.UtcNow:HH:mm:ss.ffffff} {padding} {packet.Sequence} {packet.Type} {packet.Command} (empty)");
+            }
+            else
+            {
+                var dataLength = $"({packet.Body.Data.Length} bytes)";
+                var data = BitConverter.ToString(packet.Body.Data.ToArray()).Replace("-", " ");
+                if (data.Length > 47)
+                {
+                    data = data.Substring(0, 47) + "...";
+                }
+                Console.WriteLine($"{DateTime.UtcNow:HH:mm:ss.ffffff} {padding} {packet.Sequence} {packet.Type} {packet.Command}: {dataLength}: {data}");
+            }
+        }
+    }
+}
--- DigiMixer/DigiMixer.MackieDump/Program.cs
+++ DigiMixer/DigiMixer.MackieDump/Program.cs
@@ -1,60 +1,60 @@
 ﻿using DigiMixer.MackieDump;
+using System.Net;
 
 if (args.Length == 0)
 {
     Console.WriteLine("Required command line arguments: <mode> <options>");
-    Console.WriteLine("Modes: record, dump, convert, listen");
+    Console.WriteLine("Modes: record, dump, convert, listen, info");
     Console.WriteLine("Record options: <IP address> <port - typically 50001> <output file>");
     Console.WriteLine("Dump options: <file>");
     Console.WriteLine("Convert options: <input file> <client address> <mixer address> <output file>");
     Console.WriteLine("Listen options: <input file> <client address> <mixer address> <output file>");
+    Console.WriteLine("Info options: <client address> <mixer address> <output file> <info numbers ...>");
     return;
 }
 
-if (args[0] == "record")
+switch (args[0])
 {
-    if (args.Length != 4)
-    {
-        Console.WriteLine("Record options: <IP address> <port - typically 50001> <output file>");
-        return;
-    }
-    string address = args[1];
-    int port = int.Parse(args[2]);
-    string file = args[3];
-    await Recorder.ExecuteAsync(address, port, file);
-}
-else if (args[0] == "dump")
-{
-    if (args.Length != 2)
-    {
-        Console.WriteLine("Dump options: <file>");
-        return;
-    }
-    Dumper.Execute(args[1]);
-}
-else if (args[0] == "convert")
-{
-    if (args.Length != 5)
-    {
-        Console.WriteLine("Convert options: <input file> <client address> <mixer address> <output file>");
-    }
-    Converter.Execute(args[1], args[2], args[3], args[4]);
-}
-else if (args[0] == "listen")
-{
-    if (args.Length != 4)
-    {
-        Console.WriteLine("Listen options: <IP address> <port - typically 50001> <output file>");
-        return;
-    }
-    string address = args[1];
-    int port = int.Parse(args[2]);
-    string file = args[3];
-    await Listener.ExecuteAsync(address, port, file);
-}
-else
-{
+    case "record":
+        if (args.Length != 4)
+        {
+            Console.WriteLine("Record options: <IP address> <port - typically 50001> <output file>");
+            return;
+        }
+        await Recorder.ExecuteAsync(args[1], int.Parse(args[2]), args[3]);
+        break;
+    case "dump":
+        if (args.Length != 2)
+        {
+            Console.WriteLine("Dump options: <file>");
+            return;
+        }
+        Dumper.Execute(args[1]);
+        break;
+    case "convert":
+        if (args.Length != 5)
+        {
+            Console.WriteLine("Convert options: <input file> <client address> <mixer address> <output file>");
+        }
+        Converter.Execute(args[1], args[2], args[3], args[4]);
+        break;
+    case "listen":
+        if (args.Length != 4)
+        {
+            Console.WriteLine("Listen options: <IP address> <port - typically 50001> <output file>");
+            return;
+        }
+        await Listener.ExecuteAsync(args[1], int.Parse(args[2]), args[3]);
+        break;
+    case "info":
+        if (args.Length < 4)
+        {
+            Console.WriteLine("Info options: <client address> <mixer address> <output file> <info numbers ...>");
+        }
+        await InfoRequester.ExecuteAsync(args[1], int.Parse(args[2]), args[3], args.Skip(4).Select(byte.Parse).ToList());
+        break;
+    default:
     Console.WriteLine($"Unknown mode: {args[0]}");
-    return;
+        break;
 }
 
