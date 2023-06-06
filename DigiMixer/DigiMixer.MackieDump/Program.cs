382eb01d8473c4b98771cb89f1858df505626a5e
Jon Skeet <skeet@pobox.com>
2023-06-06 07:44:12 +0100

    New tool to watch and report values for Mackie DL mixers

--- DigiMixer/DigiMixer.MackieDump/Program.cs
+++ DigiMixer/DigiMixer.MackieDump/Program.cs
@@ -4,13 +4,14 @@ using System.Net;
 if (args.Length == 0)
 {
     Console.WriteLine("Required command line arguments: <mode> <options>");
-    Console.WriteLine("Modes: record, dump, convert, listen, info, meters");
+    Console.WriteLine("Modes: record, dump, convert, listen, info, meters, watch");
     Console.WriteLine("Record options: <IP address> <port - typically 50001> <output file>");
     Console.WriteLine("Dump options: <file>");
     Console.WriteLine("Convert options: <input file> <client address> <mixer address> <output file>");
     Console.WriteLine("Listen options: <input file> <client address> <mixer address> <output file>");
     Console.WriteLine("Info options: <mixer address> <port - typically 50001> <output file> <info numbers ...>");
     Console.WriteLine("Meters options: <mixer address> <port - typically 50001> <number of meters>");
+    Console.WriteLine("Watch options: <mixer address> <port - typically 50001>");
     return;
 }
 
@@ -63,6 +64,14 @@ switch (args[0])
             return;
         }
         await MeterDisplay.ExecuteAsync(args[1], int.Parse(args[2]), int.Parse(args[3]));
+        break;
+    case "watch":
+        if (args.Length != 3)
+        {
+            Console.WriteLine("Watch options: <mixer address> <port - typically 50001>");
+            return;
+        }
+        await Watcher.ExecuteAsync(args[1], int.Parse(args[2]));
         break;
     default:
     Console.WriteLine($"Unknown mode: {args[0]}");
---
+++ DigiMixer/DigiMixer.MackieDump/Watcher.cs
@@ -0,0 +1,72 @@
+﻿using DigiMixer.Mackie.Core;
+using Google.Protobuf;
+using Microsoft.Extensions.Logging.Abstractions;
+
+namespace DigiMixer.MackieDump;
+internal class Watcher
+{
+    internal static async Task ExecuteAsync(string address, int port)
+    {
+        var cts = new CancellationTokenSource();
+        var task = Listen(address, port, cts.Token);
+        Console.WriteLine("Press return to stop listening");
+        Console.ReadLine();
+        cts.Cancel();
+        await task;
+    }
+
+    private static async Task Listen(string address, int port, CancellationToken token)
+    {
+        var controller = new MackieController(NullLogger.Instance, address, port);
+
+        controller.MapCommand(MackieCommand.ClientHandshake, _ => new byte[] { 0x10, 0x40, 0xf0, 0x1d, 0xbc, 0xa2, 0x88, 0x1c });
+        controller.MapCommand(MackieCommand.GeneralInfo, _ => new byte[] { 0, 0, 0, 2, 0, 0, 0x40, 0 });
+        controller.MapCommand(MackieCommand.ChannelInfoControl, packet => new MackiePacketBody(packet.Body.Data.Slice(0, 4)));
+        controller.MapCommand(MackieCommand.ChannelInfoControl, packet => new MackiePacketBody(packet.Body.Data.Slice(0, 4)));
+        controller.MapCommandAction(MackieCommand.ChannelValues, HandleChannelValues);
+        await controller.Connect(default);
+        controller.Start();
+
+        // Send initial packets to say "we're here and want to be notified of changes"
+        CancellationToken cancellationToken = default;
+        await controller.SendRequest(MackieCommand.KeepAlive, MackiePacketBody.Empty, cancellationToken);
+        await controller.SendRequest(MackieCommand.ClientHandshake, MackiePacketBody.Empty, cancellationToken);
+        await controller.SendRequest(MackieCommand.GeneralInfo, new byte[] { 0, 0, 0, 2 }, cancellationToken);
+
+        while (!token.IsCancellationRequested)
+        {
+            await Task.Delay(2000);
+            await controller.SendRequest(MackieCommand.KeepAlive, MackiePacketBody.Empty, cancellationToken);
+        }
+
+        controller.Dispose();
+
+        void HandleChannelValues(MackiePacket packet)
+        {
+            var body = packet.Body;
+            if (body.Length < 8)
+            {
+                return;
+            }
+            uint chunk1 = body.GetUInt32(1);
+            // TODO: Handle other value types.
+            if ((chunk1 & 0xff00) != 0x0500)
+            {
+                return;
+            }
+            // Don't display huge "all values" packet.
+            if (body.ChunkCount > 5)
+            {
+                return;
+            }
+            int start = body.GetInt32(0);
+            for (int i = 0; i < body.ChunkCount - 2; i++)
+            {
+                int address = start + i;
+                int int32Value = body.GetInt32(i + 2);
+                float singleValue = body.GetSingle(i + 2);
+                Console.WriteLine($"{address}: {int32Value} / {singleValue}");
+            }
+        }
+    }
+}
