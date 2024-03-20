c538423da4632a218f507e2d416b5d0181f90999
Jon Skeet <skeet@pobox.com>
2024-03-20 11:16:45 +0000

    Initial setup and release of 1.0.0
    Class1 contains a void parameterless method, and an Int32-returning
    property.
    
    The project targets netstandard2.1.

---
+++ CompatIssue/Class1.cs
@@ -0,0 +1,10 @@
+﻿namespace CompatIssue;
+
+public class Class1
+{
+    public void Method()
+    {
+    }
+    
+    public int Property => 10;
+}
---
+++ CompatIssue/README.md
@@ -0,0 +1,2 @@
+This directory exists to demonstrate an issue with the MS compat tool.
+See https://github.com/dotnet/sdk/issues/39629 for details
