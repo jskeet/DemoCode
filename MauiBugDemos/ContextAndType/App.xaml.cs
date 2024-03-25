f2382197572c540e1c5ab37539ddaa84c353eb09
Jon Skeet <skeet@pobox.com>
2024-03-25 19:24:34 +0000

    Repro for Maui bug

---
+++ MauiBugDemos/ContextAndType/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace ContextAndType;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ MauiBugDemos/ContextAndType/ParentViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace ContextAndType;
+
+public class ParentViewModel
+{
+    public string Text => "ParentText";
+    public ChildViewModel Child { get; } = new ChildViewModel();
+}
+
+public class ChildViewModel
+{
+    public string Text => "ChildText";
+    public string Text2 => "ChildText2";
+}
