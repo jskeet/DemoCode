f9df420f54bdf6a3038e69e36c468d941e11ab69
Jon Skeet <skeet@pobox.com>
2025-01-12 17:49:38 +0000

    Add TypeConverters.TryConvert extension method

---
+++ WpfUtil/JonSkeet.WpfUtil/TypeConverters.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel;
+
+namespace JonSkeet.WpfUtil;
+
+public static class TypeConverters
+{
+    public static bool TryConvert<T>(this TypeConverter converter, string text, out T value)
+    {
+        if (converter.IsValid(text))
+        {
+            value = (T) converter.ConvertFrom(text);
+            return true;
+        }
+        else
+        {
+            value = default(T);
+            return false;
+        }
+    }
+}
