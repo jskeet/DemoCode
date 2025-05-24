86141b2266df4ffd13ca4f4d7231f80799fabafc
Jon Skeet <skeet@pobox.com>
2025-05-24 10:46:06 +0100

    Initial project setup for Yamaha TF series
    This is completely non-functional, but means future commits will be about the actual code/protocol.

---
+++ DigiMixer/DigiMixer.TfSeries/TfMixer.cs
@@ -0,0 +1,71 @@
+﻿using DigiMixer.Core;
+using Microsoft.Extensions.Logging;
+
+namespace DigiMixer.TfSeries;
+
+public static class TfMixer
+{
+    public static IMixerApi CreateMixerApi(ILogger logger, string host, int port = 50368, MixerApiOptions? options = null) =>
+        new TfMixerApi(logger, host, port, options);
+}
+
+internal class TfMixerApi : IMixerApi
+{
+    public TfMixerApi(ILogger logger, string host, int port, MixerApiOptions? options)
+    {
+    }
+
+    public TimeSpan KeepAliveInterval => throw new NotImplementedException();
+
+    public IFaderScale FaderScale => throw new NotImplementedException();
+
+    public Task<bool> CheckConnection(CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task Connect(CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<MixerChannelConfiguration> DetectConfiguration(CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Dispose()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void RegisterReceiver(IMixerReceiver receiver)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task RequestAllData(IReadOnlyList<ChannelId> channelIds)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task SendKeepAlive()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task SetFaderLevel(ChannelId inputId, ChannelId outputId, FaderLevel level)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task SetFaderLevel(ChannelId outputId, FaderLevel level)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task SetMuted(ChannelId channelId, bool muted)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ DigiMixer/Protocols/yamaha-tf.md
@@ -0,0 +1,5 @@
+# Yamaha TF Rack protocol
+
+Currently looks very similar to the DM protocol... but rather than assume this,
+I'll implement it separately (copying code where appropriate), then see if I
+can combine the two and abstract any differences.
