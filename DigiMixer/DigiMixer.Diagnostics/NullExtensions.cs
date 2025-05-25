a9754e2c12d11038c26112108ed924e46fba5da2
Jon Skeet <skeet@pobox.com>
2025-05-25 16:31:09 +0100

    Tooling to process messages directly from Wireshark files

---
+++ DigiMixer/DigiMixer.Diagnostics/NullExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Runtime.CompilerServices;
+
+namespace DigiMixer.Diagnostics;
+
+/// <summary>
+/// Extension methods to make working with null values (and nullable reference types) simpler.
+/// </summary>
+public static class NullExtensions
+{
+    public static T OrThrow<T>([NotNull] this T? text, [CallerArgumentExpression(nameof(text))] string? message = null) where T : class =>
+        text ?? throw new InvalidDataException($"No value for '{message}'");
+
+    public static T OrThrow<T>([NotNull] this T? text, [CallerArgumentExpression(nameof(text))] string? message = null) where T : struct =>
+        text ?? throw new InvalidDataException($"No value for '{message}'");
+}
--- DigiMixer/DigiMixer.Diagnostics/WiresharkDump.cs
+++ DigiMixer/DigiMixer.Diagnostics/WiresharkDump.cs
@@ -1,4 +1,9 @@
-﻿using PcapngFile;
+﻿using DigiMixer.Core;
+using PcapngFile;
+using System.Collections.Concurrent;
+using System.Collections.Immutable;
+using System.Net;
+using System.Net.Sockets;
 
 namespace DigiMixer.Diagnostics;
 
@@ -15,10 +20,54 @@ public class WiresharkDump
 
     public static WiresharkDump Load(string filename)
     {
-        using (var reader = new Reader(filename))
+        using var reader = new Reader(filename);
+        var blocks = reader.AllBlocks.ToList();
+        return new WiresharkDump(blocks);
+    }
+
+    public async Task<ImmutableList<AnnotatedMessage<TMessage>>> ProcessMessages<TMessage>(string mixerIpAddress, string clientIpAddress) where TMessage : class, IMixerMessage<TMessage>
+    {
+        // Currently we assume a single client, a single mixer, and a single stream of messages between the two.
+        IPAddress clientAddr = IPAddress.Parse(clientIpAddress);
+        IPAddress mixerAddr = IPAddress.Parse(mixerIpAddress);
+
+        DateTime? currentTimestamp = null;
+        IPAddress? currentSource = null;
+        IPAddress? currentDestination = null;
+
+        List<AnnotatedMessage<TMessage>> messages = [];
+
+        var outboundProcessor = new MessageProcessor<TMessage>(AddMessage, 1024 * 1024);
+        var inboundProcessor = new MessageProcessor<TMessage>(AddMessage, 1024 * 1024);
+
+        foreach (var packet in IPV4Packets)
         {
-            var blocks = reader.AllBlocks.ToList();
-            return new WiresharkDump(blocks);
+            if (packet.Type != ProtocolType.Tcp)
+            {
+                continue;
+            }
+
+            currentTimestamp = packet.Timestamp;
+            currentSource = packet.Source.Address;
+            currentDestination = packet.Dest.Address;
+            if (packet.Source.Address.Equals(clientAddr) && packet.Dest.Address.Equals(mixerAddr))
+            {
+                await outboundProcessor.Process(packet.Data, default);
+            }
+            else if (packet.Source.Address.Equals(mixerAddr) && packet.Dest.Address.Equals(clientAddr))
+            {
+                await inboundProcessor.Process(packet.Data, default);
+            }
+        }
+        return [.. messages];
+
+        void AddMessage(TMessage message)
+        {
+            var direction = currentSource.OrThrow().Equals(clientAddr) ?
+                MessageDirection.ClientToMixer : MessageDirection.MixerToClient;
+            var annotated = new AnnotatedMessage<TMessage>(message, currentTimestamp.OrThrow(), direction,
+                currentSource.OrThrow(), currentDestination.OrThrow());
+            messages.Add(annotated);
         }
     }
 }
