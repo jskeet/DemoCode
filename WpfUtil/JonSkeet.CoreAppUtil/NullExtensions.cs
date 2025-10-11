f9d6ea5196350828c696c2cfb7e1a78f48c30b39
Jon Skeet <skeet@pobox.com>
2025-10-11 09:59:44 +0100

    Add NullExtensions for use more broadly
    (These are copied from election2029.uk)

---
+++ WpfUtil/JonSkeet.CoreAppUtil/NullExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Runtime.CompilerServices;
+
+namespace JonSkeet.CoreAppUtil;
+
+#nullable enable
+
+/// <summary>
+/// Extension methods to make working with null values (and nullable reference types) simpler.
+/// </summary>
+public static class NullExtensions
+{
+    public static T OrThrow<T>([NotNull] this T? text, [CallerArgumentExpression(nameof(text))] string? message = null) where T : class =>
+        text ?? throw new InvalidDataException($"No value for '{message}'");
+
+    public static T OrThrow<T>([NotNull] this T? text, [CallerArgumentExpression(nameof(text))] string? message = null) where T : struct =>
+        text ?? throw new InvalidDataException($"No value for '{message}'");
+}
