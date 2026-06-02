5fbe2a4698a1ceb6f0cb830ec89c98a255fd1195
Jon Skeet <skeet@pobox.com>
2026-06-02 10:21:08 +0100

    First part of adding descriptions to configuration schema

--- DigiMixer/DigiMixer.AppCore/ChannelMapping.cs
+++ DigiMixer/DigiMixer.AppCore/ChannelMapping.cs
@@ -1,4 +1,5 @@
 ﻿using Newtonsoft.Json;
+using System.ComponentModel;
 
 namespace DigiMixer.AppCore;
 
@@ -6,37 +7,44 @@ namespace DigiMixer.AppCore;
 /// A mapping between an configuration channel ID (text-based, user-configurable)
 /// and a DigiMixer channel ID (largely opaque integer), with an optional display name.
 /// </summary>
+[Description("Configuration for a DigiMixer channel, including a mapping from a user-facing ID (e.g. 'Lectern') to a DigiMixer numeric channel")]
 public class ChannelMapping
 {
     /// <summary>
     /// The mixer channel ID (for the relevant input or output).
     /// The precise numbers available will depend on hardware.
     /// </summary>
+    [Description("The DigiMixer numeric channel ID. Precise values will depend on the hardware; contact skeet@pobox.com for more details.")]
     public int Channel { get; set; }
 
     /// <summary>
     /// The ID used within the configuration file.
     /// </summary>
+    [Description("The identifier used to refer to this channel within other configuration, e.g. in At Your Service. Separating this logical ID from the hardware-based ID allows channels to be remapped without changing as much configuration.")]
     public string Id { get; set; }
 
     /// <summary>
     /// An optional display name.
     /// </summary>
+    [Description("Optional display name for the channel. When this isn't specified, the ID is displayed.")]
     public string DisplayName { get; set; }
 
     /// <summary>
     /// The background colour to use (or leave empty to use the default).
     /// </summary>
+    [Description("The background colour to use for the channel. Leave this empty to use a default colour.")]
     public string Color { get; set; }
 
     /// <summary>
     /// Whether the channel should be shown by default to start with.
     /// </summary>
+    [Description("Whether or not this channel should be shown in the user interface by default.")]
     public bool InitiallyVisible { get; set; }
 
     /// <summary>
     /// Optional short name to use for constrained UIs (e.g. digital scribble strips).
     /// </summary>
+    [Description("Optional short name to use for constrained UIs(e.g.digital scribble strips).")]
     public string ShortName { get; set; }
 
     /// <summary>
@@ -55,6 +63,7 @@ public class ChannelMapping
     /// For output channels, set to true if this is a foldback channel.
     /// Foldback channels can be muted/unmuted on a per-scene basis.
     /// </summary>
+    [Description("Whether this is a foldback channel (e.g. a musician's monitor).")]
     public bool Foldback { get; set; }
 
     public override string ToString() => DisplayName is null ? Id : $"{DisplayName} ({Id})";
--- DigiMixer/DigiMixer.AppCore/DigiMixerConfig.cs
+++ DigiMixer/DigiMixer.AppCore/DigiMixerConfig.cs
@@ -17,6 +17,7 @@ namespace DigiMixer.AppCore;
 /// <summary>
 /// Configuration for the mixer.
 /// </summary>
+[Description("Top-level configuration for DigiMixer, which provides integration for audio mixers.")]
 public class DigiMixerConfig
 {
     /// <summary>
@@ -30,17 +31,22 @@ public class DigiMixerConfig
     /// if the mixer is otherwise fully configured.
     /// </summary>
     [DefaultValue(true)]
+    [Description("Whether or not the mixer is enabled, primarily to allow a fake to be used.")]
     public bool Enabled { get; set; } = true;
 
+    [Description("IP address of the mixer.")]
     public string Address { get; set; }
+    [Description("TCP port of the mixer.")]
     public int? Port { get; set; }
 
+    [Description("Type of audio mixer.")]
     public MixerHardwareType HardwareType { get; set; }
 
     /// <summary>
     /// The level (0-1) at which feedback is detected.
     /// Defaults to null, in which case feedback muting is disabled.
     /// </summary>
+    [Description("When set, the level (between 0 and 1) at which feedback is detected. When not set, feedback muting is disabled.")]
     public double? FeedbackMutingThreshold { get; set; }
 
     /// <summary>
@@ -49,41 +55,49 @@ public class DigiMixerConfig
     /// Defaults to 100 (1/10th of a second).
     /// </summary>
     [DefaultValue(100)]
+    [Description("The number of milliseconds for which an input level must be at or above FeedbackMutingThreshold before the channel is automatically muted.")]
     public int FeedbackMutingMillis { get; set; } = 100;
 
     /// <summary>
     /// Name of the X-Touch Mini device as a MIDI port.
     /// </summary>
+    [Description("The MIDI port name for the X-Touch Mini device to integrate with, if any")]
     public string XTouchMiniDevice { get; set; }
 
     [DefaultValue(5)]
+    [Description("The sensitivity of the X-Touch Mini rotary encoders for volume control.")]
     public int XTouchSensitivity { get; set; } = 5;
 
     /// <summary>
     /// Whether the main fader on the X-Touch Mini should be used for the main volume.
     /// </summary>
     [DefaultValue(true)]
+    [Description("Whether the main fader (on the right) on the X-Touch Mini should be used for the main mixer volume.")]
     public bool XTouchMainVolumeEnabled { get; set; } = true;
 
     /// <summary>
     /// Name of the iCON Platform M+ device as a MIDI port.
     /// </summary>
     [JsonProperty("IconM+Device")]
+    [Description("The MIDI port name for the iCON Platform M+ device to integrate with, if any")]
     public string IconMPlusDevice { get; set; }
 
     /// <summary>
     /// Name of the iCON Platform X+ device as a MIDI port.
     /// </summary>
     [JsonProperty("IconX+Device")]
+    [Description("The MIDI port name for the iCON Platform X+ device to integrate with, if any")]
     public string IconXPlusDevice { get; set; }
 
+    [Description("Configuration for each input channel to be used. (Any irrelevant channels do not need to be configured.)")]
     public List<ChannelMapping> InputChannels { get; set; } = new List<ChannelMapping>();
 
     /// <summary>
-    /// The display names of mixer output channels. For a stereo output
+    /// The configuration of mixer output channels. For a stereo output
     /// only the lower channel number is required. An output index
     /// of 0 means "main bus".
     /// </summary>
+    [Description("Configuration for each output channel to be used. (Any irrelevant channels do not need to be configured.) The main output channel is expected to be the first entry in this list.")]
     public List<ChannelMapping> OutputChannels { get; set; } = new List<ChannelMapping>();
 
     public IMixerApi CreateMixerApi(ILogger logger, MixerApiOptions options = null)
@@ -111,24 +125,41 @@ public class DigiMixerConfig
         };
     }
 
+    [Description("The various types of audio mixer supported by DigiMixer.")]
     public enum MixerHardwareType
     {
+        [Description("A fake mixer, used for testing when the real mixer isn't available.")]
         Fake,
+        [Description("Behringer X-Air, e.g. XR-18")]
         XAir,
+        [Description("Behringer X-Air, e.g. XR-18. (This is an alias for XAir.)")]
         BehringerXAir = XAir,
         // The M32C appears to be identical to the X32 in terms of OSC.
+        [Description("Behringer X32 or M32.")]
         X32,
+        [Description("Behringer X32 or M32. (This is an alias for X32.)")]
         M32 = X32,
+        [Description("Midas M32. (This is an alias for X32.)")]
         MidasM32 = X32,
+        [Description("Behringer X32. (This is an alias for X32.)")]
         BehringerX32 = X32,
+        [Description("Soundcard Ui mixer, e.g. Ui24R")]
         SoundcraftUi,
+        [Description("Allen and Heath Qu series, e.g. Qu-SB")]
         AllenHeathQu,
+        [Description("RCF M 18")]
         RcfM18,
+        [Description("Mackie DL series, e.g. DL32R")]
         MackieDL,
+        [Description("PreSonus StudioLive series, e.g. 16R Series III")]
         StudioLive,
+        [Description("Allen and Heath CQ series, e.g. CQ20B")]
         AllenHeathCq,
+        [Description("Yamaha DM series, e.g. DM3")]
         YamahaDm,
+        [Description("Yamaha TF series, e.g. TF-Rack")]
         YamahaTf,
+        [Description("Behringer Wing")]
         BehringerWing
     }
 }
